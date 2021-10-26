714e519dc54fe720ddac8bf121ae82bd4dc44edf
ganzhab <bodaganj@gmail.com>
2021-10-27 00:07:52 +0200

    O(n) for Qualtrics task

--- src/main/java/com/coding/tasks/hackerrank/QualtricsGetSortedPermutationTask.java
+++ src/main/java/com/coding/tasks/hackerrank/QualtricsGetSortedPermutationTask.java
@@ -2,16 +2,13 @@ package com.coding.tasks.hackerrank;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 import java.util.stream.Collectors;
 
 public class QualtricsGetSortedPermutationTask {
-
-   public static void main(String[] args) {
-      System.out.println(findString("abc"));
-      System.out.println(findString("cab"));
-      System.out.println(findString("ccc"));
-   }
 
    /**
     * Find the final string which is the last (in terms of sorting) combination of letters (you are not able to change positions)
@@ -20,7 +17,49 @@ public class QualtricsGetSortedPermutationTask {
     * cab -> c, a, b, ca, cb, ab, cab -> cb
     * ccc -> c, cc, ccc ->               ccc
     */
-   public static String findString(String input) {
+   public static void main(String[] args) {
+      System.out.println(findString("abc"));
+      System.out.println(findString("cab"));
+      System.out.println(findString("ccc"));
+      // ========================
+      System.out.println();
+      System.out.println(simpleSolution("abc"));
+      System.out.println(simpleSolution("cab"));
+      System.out.println(simpleSolution("ccc"));
+   }
+
+   private static String simpleSolution(String input) {
+      StringBuilder result = new StringBuilder();
+
+      List<String> inputList = new ArrayList<>(Arrays.asList(input.split("")));
+      Map<String, List<Integer>> mapping = new TreeMap<>(Collections.reverseOrder());
+
+      for (int i = 0; i < inputList.size(); i++) {
+         String key = inputList.get(i);
+         if (mapping.containsKey(key)) {
+            mapping.get(key).add(i);
+         } else {
+            List<Integer> newList = new ArrayList<>();
+            newList.add(i);
+            mapping.put(key, newList);
+         }
+      }
+
+      int highestIndex = 0;
+
+      for (String key : mapping.keySet()) {
+         for (Integer integer : mapping.get(key)) {
+            if (highestIndex <= integer) {
+               result.append(key);
+               highestIndex = integer;
+            }
+         }
+      }
+      return result.toString();
+   }
+
+   // ================================== recursion (ugly) =======================================
+   private static String findString(String input) {
       List<String> inputList = new ArrayList<>(Arrays.asList(input.split("")));
       return runRecursion("", inputList);
    }
