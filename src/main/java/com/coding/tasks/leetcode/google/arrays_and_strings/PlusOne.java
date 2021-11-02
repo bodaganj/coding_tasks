9273572095958d52a02275d9150a848be4c33581
ganzhab <bodaganj@gmail.com>
2021-11-02 17:45:55 +0100

    Plus one is implemented

---
+++ src/main/java/com/coding/tasks/leetcode/google/arrays_and_strings/PlusOne.java
@@ -0,0 +1,50 @@
+package com.coding.tasks.leetcode.google.arrays_and_strings;
+
+import java.math.BigInteger;
+
+public class PlusOne {
+
+   public static void main(String[] args) {
+//      int[] plusOne = plusOne(new int[]{1, 2, 3});
+      int[] plusOne = plusOne(new int[]{9, 8, 7, 6, 5, 4, 3, 2, 1, 0});
+      for (int i : plusOne) {
+         System.out.println(i);
+      }
+   }
+
+   private static int[] plusOne(int[] digits) {
+      StringBuilder stringBuilder = new StringBuilder();
+      for (int digit : digits) {
+         stringBuilder.append(digit);
+      }
+
+      BigInteger finalInteger = new BigInteger(stringBuilder.toString()).add(new BigInteger("1"));
+      String finalString = String.valueOf(finalInteger);
+
+      String[] strings = finalString.split("");
+      int[] finalArray = new int[strings.length];
+
+      for (int i = 0; i < finalArray.length; i++) {
+         finalArray[i] = Integer.parseInt(strings[i]);
+      }
+      return finalArray;
+   }
+
+   // brute force int calculation
+//   private static int[] plusOne(int[] digits) {
+//      int length = digits.length;
+//
+//      for (int i = length - 1; i >= 0; --i) {
+//         if (digits[i] == 9) {
+//            digits[i] = 0;
+//         } else {
+//            digits[i]++;
+//            return digits;
+//         }
+//      }
+//
+//      digits = new int[length + 1];
+//      digits[0] = 1;
+//      return digits;
+//   }
+}
