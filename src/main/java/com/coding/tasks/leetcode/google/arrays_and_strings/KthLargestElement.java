e24f0d0502d78dce5e27c400ac4d584a34e5f6cc
ganzhab <bodaganj@gmail.com>
2021-11-07 19:27:31 +0100

    k-th largest element

---
+++ src/main/java/com/coding/tasks/leetcode/google/arrays_and_strings/KthLargestElement.java
@@ -0,0 +1,23 @@
+package com.coding.tasks.leetcode.google.arrays_and_strings;
+
+import java.util.Arrays;
+
+public class KthLargestElement {
+
+   public static void main(String[] args) {
+      System.out.println(findKthLargest(new int[]{3, 2, 1, 5, 6, 4}, 2));
+      System.out.println(findKthLargest(new int[]{3, 2, 3, 1, 2, 4, 5, 5, 6}, 4));
+   }
+
+   private static int findKthLargest(int[] nums, int k) {
+      Arrays.sort(nums);
+      int counter = 1;
+      for (int i = nums.length - 1; i >= 0; i--) {
+         if (counter == k) {
+            return nums[i];
+         }
+         counter++;
+      }
+      return 0;
+   }
+}
