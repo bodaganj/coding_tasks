b12a3582743a604be2c3667b772d9ecc14efc405
ganzhab <bodaganj@gmail.com>
2021-12-25 22:18:48 +0100

    sum of N numbers is implemented

---
+++ src/main/java/com/coding/tasks/SumOfNNumbers.java
@@ -0,0 +1,16 @@
+package com.coding.tasks;
+
+public class SumOfNNumbers {
+
+   public static void main(String[] args) {
+      System.out.println(sum(5));
+      System.out.println(sum(4));
+   }
+
+   private static int sum(int n) {
+      if (n == 1) {
+         return 1;
+      }
+      return n + sum(n - 1);
+   }
+}
