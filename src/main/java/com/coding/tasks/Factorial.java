d652639e4bba8ca727ff99293264cf51269cb6cf
ganzhab <bodaganj@gmail.com>
2021-12-25 23:23:32 +0100

    factorial is implemented

---
+++ src/main/java/com/coding/tasks/Factorial.java
@@ -0,0 +1,21 @@
+package com.coding.tasks;
+
+public class Factorial {
+
+   public static void main(String[] args) {
+      System.out.println(factorial(4));
+   }
+
+   private static int factorial(int n) {
+      int[] memo = new int[n + 1];
+      memo[1] = 1;
+      return factorialWithMemo(n, memo);
+   }
+
+   private static int factorialWithMemo(int n, int[] memo) {
+      if (memo[n] == 0) {
+         memo[n] = n * factorialWithMemo(n - 1, memo);
+      }
+      return memo[n];
+   }
+}
