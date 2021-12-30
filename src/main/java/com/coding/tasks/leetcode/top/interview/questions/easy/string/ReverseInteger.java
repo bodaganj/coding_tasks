8460b82e90b29b59ab090cb06a65d00bdc34bb00
ganzhab <bodaganj@gmail.com>
2021-12-30 22:27:28 +0100

    several string tasks are implemented

---
+++ src/main/java/com/coding/tasks/leetcode/top/interview/questions/easy/string/ReverseInteger.java
@@ -0,0 +1,48 @@
+package com.coding.tasks.leetcode.top.interview.questions.easy.string;
+
+public class ReverseInteger {
+
+   public static void main(String[] args) {
+      System.out.println(reverse(234));
+      System.out.println(reverse(-20));
+      System.out.println(reverse(0));
+      System.out.println(reverse(1534236469));
+   }
+
+   private static int reverse(int x) {
+      String string = new StringBuilder(String.valueOf(x)).reverse().toString();
+      if (string.charAt(string.length() - 1) == '-') {
+         string = "-" + string.substring(0, string.length() - 1);
+      }
+
+      try {
+         return Integer.parseInt(string);
+      } catch (NumberFormatException ex) {
+         return 0;
+      }
+   }
+
+   private static int reverse2(int x) {
+      if (x == 0) {
+         return 0;
+      }
+      char[] chars = String.valueOf(x).toCharArray();
+      StringBuilder sb = new StringBuilder();
+      for (char aChar : chars) {
+         if (aChar == '-') {
+            continue;
+         }
+         sb.append(aChar);
+      }
+      String finalString = sb.reverse().toString();
+      String noZeroAtStart = finalString.replaceAll("^(0+)", "");
+      int output;
+      try {
+         output = Integer.parseInt(noZeroAtStart);
+      } catch (NumberFormatException ex) {
+         return 0;
+      }
+
+      return chars[0] == '-' ? -1 * output : output;
+   }
+}
---
+++ src/main/java/com/coding/tasks/leetcode/top/interview/questions/easy/string/ValidAnagram.java
@@ -0,0 +1,27 @@
+package com.coding.tasks.leetcode.top.interview.questions.easy.string;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ValidAnagram {
+
+   public static void main(String[] args) {
+      System.out.println(isAnagram("anagram", "naragam"));
+      System.out.println(isAnagram("rat", "car"));
+   }
+
+   private static boolean isAnagram(String first, String second) {
+      Map<Character, Integer> firstMapping = getMapping(first);
+      Map<Character, Integer> secondMapping = getMapping(second);
+
+      return firstMapping.equals(secondMapping);
+   }
+
+   private static Map<Character, Integer> getMapping(String input) {
+      Map<Character, Integer> mapping = new HashMap<>();
+      for (char value : input.toCharArray()) {
+         mapping.put(value, mapping.getOrDefault(value, 0) + 1);
+      }
+      return mapping;
+   }
+}
