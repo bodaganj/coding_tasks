3d22f447d56fa60f23e02bacd5eef5198f7f5eb2
ganzhab <bodaganj@gmail.com>
2022-01-05 21:52:15 +0100

    NextPermutation is implemented

--- src/main/java/com/coding/tasks/leetcode/google/arrays_and_strings/ThreeSum.java
+++ src/main/java/com/coding/tasks/leetcode/google/arrays_and_strings/ThreeSum.java
@@ -2,39 +2,48 @@ package com.coding.tasks.leetcode.google.arrays_and_strings;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
 
 public class ThreeSum {
 
    public static void main(String[] args) {
-      threeSum(new int[]{-1, 0, 1, 2, -1, -4, -2, -3, 3, 0, 4});
+      List<Integer> set1 = List.of(1, 0);
+      List<Integer> set2 = List.of(0, 1);
+
+      Set<List<Integer>> set = new HashSet<>();
+      set.add(set1);
+      set.add(set2);
+      System.out.println(set.size());
+
+      System.out.println(threeSum(new int[]{-1, 0, 1, 2, -1, -4, -2, -3, 3, 0, 4}));
    }
 
    private static List<List<Integer>> threeSum(int[] nums) {
-      Arrays.sort(nums);
-      List<List<Integer>> res = new ArrayList<>();
-      for (int i = 0; i < nums.length && nums[i] <= 0; ++i) {
-         if (i == 0 || nums[i - 1] != nums[i]) {
-            twoSumII(nums, i, res);
-         }
+      if (Objects.isNull(nums) || nums.length < 3) {
+         return Collections.emptyList();
       }
-      return res;
-   }
 
-   private static void twoSumII(int[] nums, int i, List<List<Integer>> res) {
-      int lo = i + 1, hi = nums.length - 1;
-      while (lo < hi) {
-         int sum = nums[i] + nums[lo] + nums[hi];
-         if (sum < 0) {
-            ++lo;
-         } else if (sum > 0) {
-            --hi;
-         } else {
-            res.add(Arrays.asList(nums[i], nums[lo++], nums[hi--]));
-            while (lo < hi && nums[lo] == nums[lo - 1]) {
-               ++lo;
+      Set<List<Integer>> result = new HashSet<>();
+      for (int i = 0; i < nums.length; i++) {
+         Map<Integer, Integer> map = new HashMap<>();
+         int j = i + 1;
+         while (j < nums.length) {
+            if (map.containsKey(nums[j])) {
+               List<Integer> tmpList = Arrays.asList(nums[i], nums[j], nums[map.get(nums[j])]);
+               Collections.sort(tmpList);
+               result.add(tmpList);
+            } else {
+               map.put(-(nums[i] + nums[j]), j);
             }
+            j++;
          }
       }
+      return new ArrayList<>(result);
    }
 }
