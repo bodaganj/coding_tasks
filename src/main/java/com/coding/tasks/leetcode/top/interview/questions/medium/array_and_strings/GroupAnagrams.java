88b51e7d43f24b4929739d6a536473fc1d31b209
ganzhab <bodaganj@gmail.com>
2022-01-09 23:28:23 +0100

    medium level is implemented for strings and arrays

---
+++ src/main/java/com/coding/tasks/leetcode/top/interview/questions/medium/array_and_strings/GroupAnagrams.java
@@ -0,0 +1,34 @@
+package com.coding.tasks.leetcode.top.interview.questions.medium.array_and_strings;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class GroupAnagrams {
+
+   public static void main(String[] args) {
+      System.out.println(groupAnagrams(new String[]{"eat", "tea", "tan", "ate", "nat", "bat"}));
+      System.out.println(groupAnagrams(new String[]{"a"}));
+      System.out.println(groupAnagrams(new String[]{""}));
+   }
+
+   private static List<List<String>> groupAnagrams(String[] strs) {
+      Map<Map<Character, Integer>, List<String>> mapping = new HashMap<>();
+
+      for (String str : strs) {
+         char[] chars = str.toCharArray();
+         Map<Character, Integer> currentMapping = new HashMap<>();
+         for (char aChar : chars) {
+            currentMapping.put(aChar, currentMapping.getOrDefault(aChar, 0) + 1);
+         }
+
+         if (!mapping.containsKey(currentMapping)) {
+            mapping.put(currentMapping, new ArrayList<>());
+         }
+         mapping.get(currentMapping).add(str);
+      }
+
+      return new ArrayList<>(mapping.values());
+   }
+}
