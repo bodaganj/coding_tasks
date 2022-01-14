97ffba9afc6be90967d9852c365bdbb907cca2fa
ganzhab <bodaganj@gmail.com>
2022-01-14 22:29:10 +0100

    RemoveNthNodeFromEndOfList is added with O(n) space complexity (second try)

--- src/main/java/com/coding/tasks/leetcode/top/interview/questions/easy/linked_list/RemoveNthNodeFromEndOfList.java
+++ src/main/java/com/coding/tasks/leetcode/top/interview/questions/easy/linked_list/RemoveNthNodeFromEndOfList.java
@@ -1,5 +1,7 @@
 package com.coding.tasks.leetcode.top.interview.questions.easy.linked_list;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 public class RemoveNthNodeFromEndOfList {
@@ -41,67 +43,23 @@ public class RemoveNthNodeFromEndOfList {
    }
 
    private static ListNode removeNthFromEnd(ListNode head, int n) {
-      if (Objects.isNull(head)) {
+      if (head == null || head.next == null) {
          return null;
       }
+      Map<Integer, ListNode> map = new HashMap<>();
+      int counter = 0;
+      ListNode fake = new ListNode(0, head);
+      ListNode tmp = fake;
 
-      int length = getListLength(head);
-      if (length < n) {
-         return null;
-      }
-
-      if (length == n) {
-         return head.next;
-      }
-
-      int j = 1;
-      ListNode prev = null;
-      ListNode curr = head;
-      while (j <= length - n + 1) {
-         if (j == length - n + 1) {
-            prev.next = curr.next;
-         }
-         prev = curr;
-         curr = curr.next;
-
-         j++;
-      }
-
-      return head;
-   }
-
-   private static int getListLength(ListNode head) {
-      ListNode tmp = head;
-      int counter = 1;
-
-      while (tmp.next != null) {
+      while (!Objects.isNull(tmp)) {
          counter++;
+         map.put(counter, tmp);
          tmp = tmp.next;
       }
-      return counter;
-   }
 
-   /**
-    * This one should be used!!!
-    */
-//   private static ListNode removeNthFromEnd(ListNode head, int n) {
-//      if (head == null || head.next == null) {
-//         return null;
-//      }
-//      Map<Integer, ListNode> map = new HashMap<>();
-//      int counter = 0;
-//      ListNode fake = new ListNode(0, head);
-//      ListNode tmp = fake;
-//
-//      while (!Objects.isNull(tmp)) {
-//         counter++;
-//         map.put(counter, tmp);
-//         tmp = tmp.next;
-//      }
-//
-//      map.get(counter - n).next = map.get(counter - n + 1).next;
-//      return fake.next;
-//   }
+      map.get(counter - n).next = map.get(counter - n + 1).next;
+      return fake.next;
+   }
 
    static class ListNode {
 
