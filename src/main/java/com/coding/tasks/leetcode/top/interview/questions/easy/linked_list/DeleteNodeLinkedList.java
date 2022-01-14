2d844092c4f650e14dc73eadde6eff3df1c1f449
ganzhab <bodaganj@gmail.com>
2022-01-14 22:12:26 +0100

    delete node is implemented (second try)

--- src/main/java/com/coding/tasks/leetcode/top/interview/questions/easy/linked_list/DeleteNodeLinkedList.java
+++ src/main/java/com/coding/tasks/leetcode/top/interview/questions/easy/linked_list/DeleteNodeLinkedList.java
@@ -2,6 +2,27 @@ package com.coding.tasks.leetcode.top.interview.questions.easy.linked_list;
 
 public class DeleteNodeLinkedList {
 
+   public static void main(String[] args) {
+      ListNode l4 = new ListNode(4);
+      ListNode l3 = new ListNode(3, l4);
+      ListNode l2 = new ListNode(2, l3);
+      ListNode l1 = new ListNode(1, l2);
+
+      ListNode tmp = l1;
+      while (tmp != null) {
+         System.out.println(tmp.val);
+         tmp = tmp.next;
+      }
+
+      deleteNode(l3);
+
+      tmp = l1;
+      while (tmp != null) {
+         System.out.println(tmp.val);
+         tmp = tmp.next;
+      }
+   }
+
    private static void deleteNode(ListNode node) {
       node.val = node.next.val;
       node.next = node.next.next;
@@ -14,6 +35,11 @@ public class DeleteNodeLinkedList {
 
       ListNode(int x) {
          val = x;
+      }
+
+      ListNode(int x, ListNode next) {
+         val = x;
+         this.next = next;
       }
    }
 }
