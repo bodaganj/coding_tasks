2bb6aceb2702516b24627b312ef9c2ed1f718a12
ganzhab <bodaganj@gmail.com>
2022-02-06 20:38:15 +0100

    try to improve

--- src/main/java/com/coding/tasks/leetcode/amazon/trees_and_graphs/WordLadderII.java
+++ src/main/java/com/coding/tasks/leetcode/amazon/trees_and_graphs/WordLadderII.java
@@ -2,11 +2,9 @@ package com.coding.tasks.leetcode.amazon.trees_and_graphs;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 import java.util.Queue;
 import java.util.Set;
 
@@ -24,54 +22,45 @@ public class WordLadderII {
       Set<List<String>> result = new HashSet<>();
       int amountOfWords = Integer.MAX_VALUE;
 
-      Map<String, List<String>> adjacent = getAdjacent(beginWord, wordList);
-
       Queue<String> queue = new LinkedList<>();
       queue.offer(beginWord);
 
       Queue<List<String>> pathQueue = new LinkedList<>();
-      List<String> beginWordList = new ArrayList<>();
-      beginWordList.add(beginWord);
-      pathQueue.offer(beginWordList);
+      pathQueue.offer(List.of(beginWord));
 
       Queue<Set<String>> visitedQueue = new LinkedList<>();
-      Set<String> set = new HashSet<>();
-      set.add(beginWord);
-      visitedQueue.offer(set);
+      visitedQueue.offer(Set.of(beginWord));
 
       while (!queue.isEmpty()) {
          String poll = queue.poll();
          List<String> pathPoll = pathQueue.poll();
          Set<String> visitedPoll = visitedQueue.poll();
 
-         List<String> list = adjacent.get(poll);
+         if (pathPoll.size() >= amountOfWords) {
+            continue;
+         }
 
-         for (String word : list) {
-            if (!visitedPoll.contains(word)) {
-               if (word.equals(endWord)) {
-                  if (pathPoll.size() + 1 < amountOfWords) {
-                     amountOfWords = pathPoll.size() + 1;
-                     List<String> newList = new ArrayList<>(pathPoll);
-                     newList.add(word);
-                     result = new HashSet<>();
-                     result.add(newList);
-                  } else if (pathPoll.size() + 1 == amountOfWords) {
-                     List<String> newList = new ArrayList<>(pathPoll);
-                     newList.add(word);
-                     result.add(newList);
-                  }
-               } else {
-                  if (pathPoll.size() + 1 < amountOfWords) {
-                     queue.offer(word);
+         for (String word : getAdjacent(poll, wordList, visitedPoll)) {
+            List<String> newList = new ArrayList<>(pathPoll);
+            newList.add(word);
 
-                     List<String> newList = new ArrayList<>(pathPoll);
-                     newList.add(word);
-                     pathQueue.offer(newList);
+            if (word.equals(endWord)) {
+               if (newList.size() < amountOfWords) {
+                  amountOfWords = newList.size();
+                  result = new HashSet<>();
+                  result.add(newList);
+               } else if (newList.size() == amountOfWords) {
+                  result.add(newList);
+               }
+            } else {
+               if (newList.size() < amountOfWords) {
+                  queue.offer(word);
 
-                     Set<String> newVisited = new HashSet<>(visitedPoll);
-                     newVisited.add(word);
-                     visitedQueue.offer(newVisited);
-                  }
+                  pathQueue.offer(newList);
+
+                  Set<String> newVisited = new HashSet<>(visitedPoll);
+                  newVisited.add(word);
+                  visitedQueue.offer(newVisited);
                }
             }
          }
@@ -80,29 +69,17 @@ public class WordLadderII {
       return new ArrayList<>(result);
    }
 
-   private static Map<String, List<String>> getAdjacent(String beginWord, List<String> wordList) {
-      Map<String, List<String>> mapping = new HashMap<>();
+   private static List<String> getAdjacent(String str, List<String> wordList, Set<String> visited) {
+      List<String> list = new ArrayList<>();
       for (String word : wordList) {
-         if (areSimilar(beginWord, word)) {
-            List<String> tmp = mapping.getOrDefault(beginWord, new ArrayList<>());
-            tmp.add(word);
-            mapping.put(beginWord, tmp);
-         }
-      }
-
-      for (int i = 0; i < wordList.size(); i++) {
-         String current = wordList.get(i);
-
-         for (String word : wordList) {
-            if (!current.equals(word) && areSimilar(current, word)) {
-               List<String> tmp = mapping.getOrDefault(current, new ArrayList<>());
-               tmp.add(word);
-               mapping.put(current, tmp);
+         if (!visited.contains(word) && !str.equals(word)) {
+            if (areSimilar(str, word)) {
+               list.add(word);
             }
          }
       }
 
-      return mapping;
+      return list;
    }
 
    private static boolean areSimilar(String word1, String word2) {
