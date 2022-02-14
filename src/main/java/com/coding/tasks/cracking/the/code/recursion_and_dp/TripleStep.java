b94f59a39f1e4f6142737f6695e89ebdf2d54159
ganzhab <bodaganj@gmail.com>
2022-02-14 21:32:57 +0100

    Climbing stairs is simplified, but still TLE

--- src/main/java/com/coding/tasks/cracking/the/code/recursion_and_dp/TripleStep.java
+++ src/main/java/com/coding/tasks/cracking/the/code/recursion_and_dp/TripleStep.java
@@ -1,37 +1,29 @@
 package com.coding.tasks.cracking.the.code.recursion_and_dp;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
 public class TripleStep {
 
-   private static Set<List<Integer>> combinations;
+   private static int counter;
 
    public static void main(String[] args) {
       System.out.println(getDifferentWays(3));
+      System.out.println(getDifferentWays(5));
    }
 
    private static int getDifferentWays(int n) {
-      combinations = new HashSet<>();
-      claim(n, new ArrayList<>());
-      return combinations.size();
+      counter = 0;
+      claim(n);
+      return counter;
    }
 
-   private static void claim(int n, List<Integer> currentSteps) {
+   private static void claim(int n) {
       if (n == 0) {
-         combinations.add(currentSteps);
+         counter++;
       } else {
          if (n - 1 >= 0) {
-            List<Integer> tmp = new ArrayList<>(currentSteps);
-            tmp.add(1);
-            claim(n - 1, tmp);
+            claim(n - 1);
          }
          if (n - 2 >= 0) {
-            List<Integer> tmp = new ArrayList<>(currentSteps);
-            tmp.add(2);
-            claim(n - 2, tmp);
+            claim(n - 2);
          }
       }
    }
