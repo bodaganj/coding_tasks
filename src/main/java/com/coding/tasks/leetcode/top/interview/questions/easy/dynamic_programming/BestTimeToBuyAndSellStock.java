e9c4d1b3d5b52e063611a151eda37918b2eb96b6
ganzhab <bodaganj@gmail.com>
2022-02-18 22:49:19 +0100

    easy dp are implemented

---
+++ src/main/java/com/coding/tasks/leetcode/top/interview/questions/easy/dynamic_programming/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,23 @@
+package com.coding.tasks.leetcode.top.interview.questions.easy.dynamic_programming;
+
+public class BestTimeToBuyAndSellStock {
+
+   public static void main(String[] args) {
+      System.out.println(maxProfit(new int[]{7, 1, 5, 3, 6, 4}));
+   }
+
+   private static int maxProfit(int[] prices) {
+      if (prices == null || prices.length == 0) {
+         return 0;
+      }
+
+      int min = prices[0];
+      int bestSell = 0;
+      for (int i = 1; i < prices.length; i++) {
+         min = Math.min(min, prices[i]);
+         bestSell = Math.max(bestSell, prices[i] - min);
+      }
+
+      return bestSell;
+   }
+}
