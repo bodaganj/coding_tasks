71df9224031dc4985d8ed8bb9bef625ce78c6d27
ganzhab <bodaganj@gmail.com>
2022-02-21 23:53:42 +0100

    first dp hard problem is partially implemented

---
+++ src/main/java/com/coding/tasks/leetcode/top/interview/questions/hard/dynamic_programming/MaximumProductSubarray.java
@@ -0,0 +1,27 @@
+package com.coding.tasks.leetcode.top.interview.questions.hard.dynamic_programming;
+
+public class MaximumProductSubarray {
+
+   public static void main(String[] args) {
+      System.out.println(maxProduct(new int[]{2, 3, -2, 4}));
+      System.out.println(maxProduct(new int[]{-2, 0, -1}));
+      System.out.println(maxProduct(new int[]{0, 2}));
+      System.out.println(maxProduct(new int[]{-2, 3, -4}));
+      System.out.println(maxProduct(new int[]{3, -1, 4}));
+      System.out.println(maxProduct(new int[]{2, -5, -2, -4, 3}));
+   }
+
+   private static int maxProduct(int[] nums) {
+      int ans = Integer.MIN_VALUE;
+
+      for (int i = 0; i < nums.length; i++) {
+         ans = Math.max(ans, nums[i]);
+         int current = 1;
+         for (int j = i + 1; j < nums.length; j++) {
+            current *= nums[j];
+            ans = Math.max(ans, nums[i] * current);
+         }
+      }
+      return ans;
+   }
+}
