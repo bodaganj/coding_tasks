e7ccdbdb62939505f4ea870455774e30f9a41115
ganzhab <bodaganj@gmail.com>
2022-02-26 22:24:50 +0100

    MaximumSubArray is implemented

--- src/main/java/com/coding/tasks/leetcode/google/dynamic_programming/MaximumSubArray.java
+++ src/main/java/com/coding/tasks/leetcode/google/dynamic_programming/MaximumSubArray.java
@@ -1,24 +1,23 @@
 package com.coding.tasks.leetcode.google.dynamic_programming;
 
+import java.util.Arrays;
+
 public class MaximumSubArray {
 
-   private static MaximumSubArray maximumSubArray = new MaximumSubArray();
-
    public static void main(String[] args) {
-      System.out.println(maximumSubArray.maxSubArray(new int[] {-2,1,-3,4,-1,2,1,-5,4}));
-      System.out.println(maximumSubArray.maxSubArray(new int[] {1}));
-      System.out.println(maximumSubArray.maxSubArray(new int[] {5,4,-1,7,8}));
+      System.out.println(maxSubArray(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
+      System.out.println(maxSubArray(new int[]{1}));
+      System.out.println(maxSubArray(new int[]{5, 4, -1, 7, 8}));
    }
 
-   public int maxSubArray(int[] nums) {
-      int currentMax = nums[0];
-      int max = nums[0];
+   private static int maxSubArray(int[] nums) {
+      int[] dp = new int[nums.length];
+      dp[0] = nums[0];
 
       for (int i = 1; i < nums.length; i++) {
-         int current = nums[i];
-         currentMax = Math.max(current, currentMax + current);
-         max = Math.max(max, currentMax);
+         dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
       }
-      return max;
+
+      return Arrays.stream(dp).max().getAsInt();
    }
 }
