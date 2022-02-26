0f8c93366389ef2d8bd2147e5fbe0ae081be6507
ganzhab <bodaganj@gmail.com>
2022-02-26 22:47:17 +0100

    BestTimeToBuyAndSell is implemented

--- src/main/java/com/coding/tasks/leetcode/google/dynamic_programming/BestTimeToBuyAndSell.java
+++ src/main/java/com/coding/tasks/leetcode/google/dynamic_programming/BestTimeToBuyAndSell.java
@@ -2,26 +2,24 @@ package com.coding.tasks.leetcode.google.dynamic_programming;
 
 public class BestTimeToBuyAndSell {
 
-   private static BestTimeToBuyAndSell sdf = new BestTimeToBuyAndSell();
-
    public static void main(String[] args) {
-      System.out.println(sdf.maxProfit(new int[]{7, 1, 5, 3, 6, 4}));
-      System.out.println(sdf.maxProfit(new int[]{7, 6, 4, 3, 1}));
-      System.out.println(sdf.maxProfit(new int[]{3, 2, 6, 5, 0, 3}));
+      System.out.println(maxProfit(new int[]{7, 1, 5, 3, 6, 4}));
+      System.out.println(maxProfit(new int[]{7, 6, 4, 3, 1}));
+      System.out.println(maxProfit(new int[]{3, 2, 6, 5, 0, 3}));
    }
 
-   public int maxProfit(int[] prices) {
-      if (prices.length < 2) {
-         return 0;
-      }
-      int lowest = prices[0];
-      int max = Integer.MIN_VALUE;
+   private static int maxProfit(int[] prices) {
+      int minPrice = prices[0];
+      int profit = Integer.MIN_VALUE;
 
       for (int i = 1; i < prices.length; i++) {
-         lowest = Math.min(lowest, prices[i]);
-         max = Math.max(max, prices[i] - lowest);
+         if (minPrice > prices[i]) {
+            minPrice = prices[i];
+         } else if (prices[i] - minPrice > profit) {
+            profit = prices[i] - minPrice;
+         }
       }
 
-      return Math.max(max, 0);
+      return Math.max(profit, 0);
    }
 }
