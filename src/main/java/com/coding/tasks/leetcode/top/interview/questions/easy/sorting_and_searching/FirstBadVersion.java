637a3ce3b059a401454b0b205c2c3b531dcdd9ff
ganzhab <bodaganj@gmail.com>
2022-03-04 23:39:20 +0100

    medium sorting and searching tasks are started to be implemented

---
+++ src/main/java/com/coding/tasks/leetcode/top/interview/questions/easy/sorting_and_searching/FirstBadVersion.java
@@ -0,0 +1,35 @@
+package com.coding.tasks.leetcode.top.interview.questions.easy.sorting_and_searching;
+
+public class FirstBadVersion {
+
+   private static int firstBadVersion(int n) {
+      int left = 1;
+      int right = n;
+
+      while (left <= right) {
+         int middle = left + (right - left) / 2;
+
+         if (isBadVersion(middle)) {
+            if (middle - 1 == 0) {
+               return middle;
+            } else if (isBadVersion(middle - 1)) {
+               right = middle - 1;
+            } else {
+               return middle;
+            }
+         } else {
+            if (isBadVersion(middle + 1)) {
+               return middle + 1;
+            } else {
+               left = middle + 1;
+            }
+         }
+      }
+
+      return n;
+   }
+
+   private static boolean isBadVersion(int n) {
+      return true;
+   }
+}
