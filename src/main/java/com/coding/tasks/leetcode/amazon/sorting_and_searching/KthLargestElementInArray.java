5eab057c4a2b628841415db1322b23d1b03264c2
ganzhab <bodaganj@gmail.com>
2022-03-07 22:43:00 +0100

    one more meeting rooms task is implemented

---
+++ src/main/java/com/coding/tasks/leetcode/amazon/sorting_and_searching/KthLargestElementInArray.java
@@ -0,0 +1,23 @@
+package com.coding.tasks.leetcode.amazon.sorting_and_searching;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class KthLargestElementInArray {
+
+   public static void main(String[] args) {
+
+   }
+
+   private static int findKthLargest(int[] nums, int k) {
+      Queue<Integer> queue = new PriorityQueue<>();
+
+      for (int num : nums) {
+         queue.offer(num);
+         if (queue.size() > k) {
+            queue.poll();
+         }
+      }
+      return queue.poll();
+   }
+}
--- src/main/java/com/coding/tasks/leetcode/amazon/sorting_and_searching/TwoSum2InputArrayIsSorted.java
+++ src/main/java/com/coding/tasks/leetcode/amazon/sorting_and_searching/TwoSum2InputArrayIsSorted.java
@@ -6,6 +6,8 @@ public class TwoSum2InputArrayIsSorted {
 
    public static void main(String[] args) {
       System.out.println(Arrays.toString(twoSum(new int[]{2, 7, 11, 15}, 9)));
+      System.out.println(Arrays.toString(twoSum(new int[]{5, 25, 75}, 100)));
+      System.out.println(Arrays.toString(twoSum(new int[]{2, 3, 4}, 6)));
    }
 
    private static int[] twoSum(int[] numbers, int target) {
@@ -13,10 +15,9 @@ public class TwoSum2InputArrayIsSorted {
       numbers[0] = target - numbers[0];
 
       while (i < numbers.length) {
-         for (int j = 0; j < i; j++) {
-            if (numbers[j] == numbers[i]) {
-               return new int[]{j + 1, i + 1};
-            }
+         int index = getIndex(numbers, numbers[i], 0, i - 1);
+         if (index != -1) {
+            return new int[]{index + 1, i + 1};
          }
 
          numbers[i] = target - numbers[i];
@@ -24,5 +25,21 @@ public class TwoSum2InputArrayIsSorted {
       }
 
       return new int[2];
+   }
+
+   private static int getIndex(int[] numbers, int target, int left, int right) {
+      while (left <= right) {
+         int mid = left + (right - left) / 2;
+
+         if (numbers[mid] == target) {
+            return mid;
+         } else if (numbers[mid] > target) {
+            left = mid + 1;
+         } else {
+            right = mid - 1;
+         }
+      }
+
+      return -1;
    }
 }
