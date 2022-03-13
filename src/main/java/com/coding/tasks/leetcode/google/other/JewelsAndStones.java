717529a2838e4d9b444e6e55035968d2ccc05a47
ganzhab <bodaganj@gmail.com>
2022-03-13 23:14:51 +0100

    several others Google tasks were implemented

---
+++ src/main/java/com/coding/tasks/leetcode/google/other/JewelsAndStones.java
@@ -0,0 +1,27 @@
+package com.coding.tasks.leetcode.google.other;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class JewelsAndStones {
+
+   public static void main(String[] args) {
+
+   }
+
+   private static int numJewelsInStones(String jewels, String stones) {
+      Set<Character> jSet = new HashSet<>();
+      for (char aChar : jewels.toCharArray()) {
+         jSet.add(aChar);
+      }
+
+      int count = 0;
+      for (char aChar : stones.toCharArray()) {
+         if (jSet.contains(aChar)) {
+            count++;
+         }
+      }
+
+      return count;
+   }
+}
