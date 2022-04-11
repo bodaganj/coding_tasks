f47b5ccf936704a8a88addf79235eae09f2beec6
ganzhab <bodaganj@gmail.com>
2022-04-11 14:44:05 +0200

    SetMatrixZeroes is implemented

---
+++ src/main/java/com/coding/tasks/leetcode/second_round/top_interview_questions/medium/array_and_strings/SetMatrixZeroes.java
@@ -0,0 +1,86 @@
+package com.coding.tasks.leetcode.second_round.top_interview_questions.medium.array_and_strings;
+
+import java.util.Arrays;
+
+public class SetMatrixZeroes {
+
+   public static void main(String[] args) {
+//      int[][] arr1 = new int[][]{
+//         {1, 1, 1, 1, 1},
+//         {1, 0, 1, 1, 1},
+//         {1, 1, 1, 0, 1}
+//      };
+//      setZeroes(arr1);
+//      System.out.println(Arrays.deepToString(arr1));
+
+//      int[][] arr2 = new int[][]{
+//         {0, 1, 1, 0},
+//         {1, 1, 1, 1},
+//         {1, 1, 1, 1}
+//      };
+//      setZeroes(arr2);
+//      System.out.println(Arrays.deepToString(arr2));
+
+      int[][] arr3 = new int[][]{
+         {1, 1, 1, 1},
+         {1, 0, 1, 1},
+         {0, 1, 1, 1},
+         {1, 1, 1, 0}
+      };
+      setZeroes(arr3);
+      System.out.println(Arrays.deepToString(arr3));
+   }
+
+   private static void setZeroes(int[][] matrix) {
+      int[] zero = findZero(matrix);
+      if (zero.length != 0) {
+         int row = zero[0];
+         int column = zero[1];
+
+         for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+               if (matrix[i][j] == 0) {
+                  matrix[i][column] = 0;
+                  matrix[row][j] = 0;
+               }
+            }
+         }
+
+         // set 0 to all rows/columns except the initial one
+         for (int i = 0; i < matrix.length; i++) {
+            if (i != row) {
+               for (int j = 0; j < matrix[0].length; j++) {
+                  if (j != column) {
+                     if (matrix[row][j] == 0) {
+                        matrix[i][j] = 0;
+                     }
+                     if (matrix[i][column] == 0) {
+                        matrix[i][j] = 0;
+                     }
+                  }
+               }
+            }
+         }
+
+         // set 0 to initial one
+         for (int i = 0; i < matrix[0].length; i++) {
+            matrix[row][i] = 0;
+         }
+         for (int i = 0; i < matrix.length; i++) {
+            matrix[i][column] = 0;
+         }
+      }
+   }
+
+   private static int[] findZero(int[][] matrix) {
+      for (int i = 0; i < matrix.length; i++) {
+         for (int j = 0; j < matrix[i].length; j++) {
+            if (matrix[i][j] == 0) {
+               return new int[]{i, j};
+            }
+         }
+      }
+
+      return new int[0];
+   }
+}
--- src/main/java/com/coding/tasks/leetcode/second_round/top_interview_questions/medium/array_and_strings/ThreeSum.java
+++ src/main/java/com/coding/tasks/leetcode/second_round/top_interview_questions/medium/array_and_strings/ThreeSum.java
@@ -1,12 +1,34 @@
 package com.coding.tasks.leetcode.second_round.top_interview_questions.medium.array_and_strings;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class ThreeSum {
 
    public static void main(String[] args) {
-//      int[] array = new int[]{-1, 0, 1, 2, -1, -4};
-//      System.out.println(threeSum(array));
-//   }
-//
-//   private static List<List<Integer>> threeSum(int[] nums) {
+      int[] array = new int[]{-1, 0, 1, 2, -1, -4};
+      System.out.println(threeSum(array));
+   }
+
+   private static List<List<Integer>> threeSum(int[] nums) {
+      Set<List<Integer>> ans = new HashSet<>();
+      for (int i = 0; i < nums.length - 2; i++) { // O(n)
+         Set<Integer> mapping = new HashSet<>();
+         for (int j = i + 1; j < nums.length; j++) { // O(n)
+            if (mapping.contains(nums[j])) {
+               List<Integer> collect = Stream.of(nums[i], nums[j], (nums[i] + nums[j]) * -1).sorted().collect(Collectors.toList());
+               ans.add(collect);
+               mapping.remove(nums[j]);
+            } else {
+               mapping.add(nums[i] * -1 - nums[j]);
+            }
+         }
+      }
+
+      return new ArrayList<>(ans);
    }
 }
