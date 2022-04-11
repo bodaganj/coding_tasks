46680022ee1df72c79bed323cb0e2a49fd9df150
ganzhab <bodaganj@gmail.com>
2022-04-12 00:31:13 +0200

    two hard questions are implemented

--- src/main/java/com/coding/tasks/leetcode/second_round/top_interview_questions/hard/strings_and_arrays/ProductArrayExceptSelf.java
+++ src/main/java/com/coding/tasks/leetcode/second_round/top_interview_questions/hard/strings_and_arrays/ProductArrayExceptSelf.java
@@ -1,12 +1,33 @@
 package com.coding.tasks.leetcode.second_round.top_interview_questions.hard.strings_and_arrays;
 
+import java.util.Arrays;
+
 public class ProductArrayExceptSelf {
 
    public static void main(String[] args) {
-//      System.out.println(Arrays.toString(productExceptSelf(new int[]{1, 2, 3, 4})));
-//      System.out.println(Arrays.toString(productExceptSelf(new int[]{-1, 1, 0, -3, 3})));
-//   }
-//
-//   private static int[] productExceptSelf(int[] nums) {
+      System.out.println(Arrays.toString(productExceptSelf(new int[]{1, 2, 3, 4})));
+   }
+
+   private static int[] productExceptSelf(int[] nums) {
+      int[] ans = new int[nums.length];
+      ans[0] = 1;
+
+      int counter = 1;
+      int product = 1;
+      while (counter < ans.length) {
+         product *= nums[counter - 1];
+         ans[counter] = product;
+         counter++;
+      }
+
+      product = 1;
+      counter = ans.length - 1;
+      while (counter >= 0) {
+         ans[counter] = ans[counter] * product;
+         product *= nums[counter];
+         counter--;
+      }
+
+      return ans;
    }
 }
--- src/main/java/com/coding/tasks/leetcode/second_round/top_interview_questions/hard/strings_and_arrays/SpiralMatrix.java
+++ src/main/java/com/coding/tasks/leetcode/second_round/top_interview_questions/hard/strings_and_arrays/SpiralMatrix.java
@@ -1,17 +1,50 @@
 package com.coding.tasks.leetcode.second_round.top_interview_questions.hard.strings_and_arrays;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SpiralMatrix {
 
+   private static int[][] direction = new int[][]{
+      {0, 1}, // right
+      {1, 0}, // down
+      {0, -1}, // left
+      {-1, 0} // up
+   };
+
    public static void main(String[] args) {
-//      System.out.println(spiralOrder(new int[][]{
-//         {1, 2, 3, 4},
-//         {5, 6, 7, 8},
-//         {9, 10, 11, 12}
-//      }));
-//
-//      System.out.println(spiralOrder(new int[][]{{3}, {2}}));
-//   }
-//
-//   private static List<Integer> spiralOrder(int[][] matrix) {
+      System.out.println(spiralOrder(new int[][]{
+         {1, 2, 3, 4},
+         {5, 6, 7, 8},
+         {9, 10, 11, 12},
+         {13, 14, 15, 16}
+      }));
+
+      System.out.println(spiralOrder(new int[][]{{3}, {2}}));
+   }
+
+   private static List<Integer> spiralOrder(int[][] matrix) {
+      List<Integer> ans = new ArrayList<>();
+      dfs(matrix, 0, 0, new int[]{0, 1}, ans);
+      return ans;
+   }
+
+   private static void dfs(int[][] matrix, int i, int j, int[] defaultDirection, List<Integer> ans) {
+      if (i >= 0 && i < matrix.length && j >= 0 && j < matrix[0].length && matrix[i][j] != -1000) {
+         ans.add(matrix[i][j]);
+         matrix[i][j] = -1000;
+
+         int potentialI = i + defaultDirection[0];
+         int potentialJ = j + defaultDirection[1];
+
+         if (potentialI >= 0 && potentialI < matrix.length && potentialJ >= 0 && potentialJ < matrix[0].length
+            && matrix[potentialI][potentialJ] != -1000) {
+            dfs(matrix, potentialI, potentialJ, defaultDirection, ans);
+         } else {
+            for (int[] ints : direction) {
+               dfs(matrix, i + ints[0], j + ints[1], ints, ans);
+            }
+         }
+      }
    }
 }
