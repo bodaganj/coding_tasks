069fa40f0cca9a96bdc1fe83b7eae6c6ef47beda
ganzhab <bodaganj@gmail.com>
2022-04-14 14:56:20 +0200

    all google interview-process questions are implemented

--- src/main/java/com/coding/tasks/leetcode/second_round/google/interview_process/FruitsIntoBaskets.java
+++ src/main/java/com/coding/tasks/leetcode/second_round/google/interview_process/FruitsIntoBaskets.java
@@ -1,17 +1,42 @@
 package com.coding.tasks.leetcode.second_round.google.interview_process;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class FruitsIntoBaskets {
 
    public static void main(String[] args) {
-//      System.out.println(totalFruit(new int[]{0, 1, 1, 4, 3}));
-//      System.out.println(totalFruit(new int[]{0, 1, 6, 6, 4, 4, 6}));
-//      System.out.println(totalFruit(new int[]{1, 0, 1, 4, 1, 4, 1, 2, 3}));
-//      System.out.println(totalFruit(new int[]{1, 2, 1}));
-//      System.out.println(totalFruit(new int[]{0, 1, 2, 2}));
-//      System.out.println(totalFruit(new int[]{1, 2, 3, 2, 2}));
-//      System.out.println(totalFruit(new int[]{3, 3, 3, 1, 2, 1, 1, 2, 3, 3, 4}));
-//   }
-//
-//   private static int totalFruit(int[] fruits) {
+      System.out.println(totalFruit(new int[]{0, 1, 1, 4, 3}));
+      System.out.println(totalFruit(new int[]{0, 1, 6, 6, 4, 4, 6}));
+      System.out.println(totalFruit(new int[]{1, 0, 1, 4, 1, 4, 1, 2, 3}));
+      System.out.println(totalFruit(new int[]{1, 2, 1}));
+      System.out.println(totalFruit(new int[]{0, 1, 2, 2}));
+      System.out.println(totalFruit(new int[]{1, 2, 3, 2, 2}));
+      System.out.println(totalFruit(new int[]{3, 3, 3, 1, 2, 1, 1, 2, 3, 3, 4}));
+   }
+
+   private static int totalFruit(int[] fruits) {
+      int max = 1;
+      Map<Integer, Integer> map = new HashMap<>();
+      int left = 0;
+      int right = 0;
+      while (right < fruits.length) {
+         int fruit = fruits[right];
+         if (map.keySet().size() < 2 || map.containsKey(fruit)) {
+            map.put(fruit, map.getOrDefault(fruit, 0) + 1);
+            right++;
+         } else {
+            while (map.keySet().size() == 2) {
+               map.put(fruits[left], map.get(fruits[left]) - 1);
+               if (map.get(fruits[left]) == 0) {
+                  map.remove(fruits[left]);
+               }
+               left++;
+            }
+         }
+         max = Math.max(max, map.values().stream().mapToInt(Integer::intValue).sum());
+      }
+
+      return max;
    }
 }
--- src/main/java/com/coding/tasks/leetcode/second_round/google/interview_process/LicenseKeyFormatting.java
+++ src/main/java/com/coding/tasks/leetcode/second_round/google/interview_process/LicenseKeyFormatting.java
@@ -3,10 +3,27 @@ package com.coding.tasks.leetcode.second_round.google.interview_process;
 public class LicenseKeyFormatting {
 
    public static void main(String[] args) {
-//      System.out.println(licenseKeyFormatting("5F3Z-2e-9-w", 4));
-//      System.out.println(licenseKeyFormatting("2-5g-3-J", 2));
-//   }
-//
-//   private static String licenseKeyFormatting(String s, int k) {
+      System.out.println(licenseKeyFormatting("5F3Z-2e-9-w", 4));
+      System.out.println(licenseKeyFormatting("2-5g-3-J", 2));
+   }
+
+   private static String licenseKeyFormatting(String s, int k) {
+      StringBuilder sb = new StringBuilder();
+      char[] chars = s.toCharArray();
+      int counter = 0;
+      for (int i = chars.length - 1; i >= 0; i--) {
+         String curr = chars[i] + "";
+         if (!curr.equals("-")) {
+            if (counter < k) {
+               sb.append(curr);
+               counter++;
+            } else {
+               counter = 1;
+               sb.append("-").append(curr);
+            }
+         }
+      }
+
+      return sb.reverse().toString().toUpperCase();
    }
 }
