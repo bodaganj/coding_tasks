a3448c103bfbf26db3156b1f39ec7a22ba36e2a8
ganzhab <bodaganj@gmail.com>
2022-04-14 13:01:40 +0200

    OddEvenJumps is mainly implemented

--- src/main/java/com/coding/tasks/leetcode/second_round/google/interview_process/OddEvenJumps.java
+++ src/main/java/com/coding/tasks/leetcode/second_round/google/interview_process/OddEvenJumps.java
@@ -1,17 +1,44 @@
 package com.coding.tasks.leetcode.second_round.google.interview_process;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 public class OddEvenJumps {
 
    public static void main(String[] args) {
-//      int[] first = new int[]{10, 13, 12, 14, 15};
-//      int[] second = new int[]{2, 3, 1, 1, 4};
-//      int[] third = new int[]{5, 1, 3, 4, 2};
-//
-//      System.out.println(oddEvenJumps(first));
-//      System.out.println(oddEvenJumps(second));
-//      System.out.println(oddEvenJumps(third));
-//   }
-//
-//   private static int oddEvenJumps(int[] arr) {
+      int[] first = new int[]{10, 13, 12, 14, 15};
+      int[] second = new int[]{2, 3, 1, 1, 4};
+      int[] third = new int[]{5, 1, 3, 4, 2};
+
+      System.out.println(oddEvenJumps(first));
+      System.out.println(oddEvenJumps(second));
+      System.out.println(oddEvenJumps(third));
+   }
+
+   private static int oddEvenJumps(int[] arr) {
+      int ans = 1;
+      boolean[] oddFlag = new boolean[arr.length];
+      boolean[] evenFlag = new boolean[arr.length];
+      oddFlag[oddFlag.length - 1] = true;
+      evenFlag[evenFlag.length - 1] = true;
+
+      for (int i = arr.length - 2; i >= 0; i--) {
+         TreeMap<Integer, Integer> tree = new TreeMap<>();
+         for (int j = i + 1; j < arr.length; j++) {
+            tree.put(arr[j], j);
+         }
+
+         Map.Entry<Integer, Integer> oddEntry = tree.ceilingEntry(arr[i]);
+         if (oddEntry != null && evenFlag[oddEntry.getValue()]) {
+            oddFlag[i] = true;
+            ans++;
+         }
+
+         Map.Entry<Integer, Integer> evenEntry = tree.floorEntry(arr[i]);
+         if (evenEntry != null && oddFlag[evenEntry.getValue()]) {
+            evenFlag[i] = true;
+         }
+      }
+      return ans;
    }
 }
--- src/main/java/com/coding/tasks/leetcode/second_round/google/interview_process/UniqueEmailAddresses.java
+++ src/main/java/com/coding/tasks/leetcode/second_round/google/interview_process/UniqueEmailAddresses.java
@@ -1,15 +1,39 @@
 package com.coding.tasks.leetcode.second_round.google.interview_process;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class UniqueEmailAddresses {
 
    public static void main(String[] args) {
-//      String[] first = new String[]{"test.email+alex@leetcode.com", "test.e.mail+bob.cathy@leetcode.com", "testemail+david@lee.tcode.com"};
-//      String[] second = new String[]{"a@leetcode.com", "b@leetcode.com", "c@leetcode.com"};
-//
-//      System.out.println(numUniqueEmails(first));
-//      System.out.println(numUniqueEmails(second));
-//   }
-//
-//   private static int numUniqueEmails(String[] emails) {
+      String[] first = new String[]{"test.email+alex@leetcode.com", "test.e.mail+bob.cathy@leetcode.com", "testemail+david@lee.tcode.com"};
+      String[] second = new String[]{"a@leetcode.com", "b@leetcode.com", "c@leetcode.com"};
+
+      System.out.println(numUniqueEmails(first));
+      System.out.println(numUniqueEmails(second));
+   }
+
+   private static int numUniqueEmails(String[] emails) {
+      Set<String> uniqueEmails = new HashSet<>();
+
+      for (String email : emails) {
+         String[] splitEmail = email.split("@");
+         String localName = getLocalName(splitEmail[0]);
+         uniqueEmails.add(localName + "@" + splitEmail[1]);
+      }
+
+      return uniqueEmails.size();
+   }
+
+   private static String getLocalName(String str) {
+      StringBuilder sb = new StringBuilder();
+      for (char aChar : str.toCharArray()) {
+         if (Character.isLetter(aChar)) {
+            sb.append(aChar);
+         } else if (aChar == '+') {
+            return sb.toString();
+         }
+      }
+      return sb.toString();
    }
 }
