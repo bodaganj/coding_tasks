d51e38fe475ac5f431251079ebb8073dfa402def
ganzhab <bodaganj@gmail.com>
2022-04-16 22:08:25 +0200

    fixed after checking solutions

--- src/main/java/com/coding/tasks/leetcode/second_round/google/arrays_and_strings/MinimumCostHireKWorkers.java
+++ src/main/java/com/coding/tasks/leetcode/second_round/google/arrays_and_strings/MinimumCostHireKWorkers.java
@@ -1,6 +1,9 @@
 package com.coding.tasks.leetcode.second_round.google.arrays_and_strings;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class MinimumCostHireKWorkers {
 
@@ -9,30 +12,70 @@ public class MinimumCostHireKWorkers {
       System.out.println(mincostToHireWorkers(new int[]{3, 1, 10, 10, 1}, new int[]{4, 8, 2, 2, 7}, 3));
    }
 
-   private static double mincostToHireWorkers(int[] quality, int[] wage, int k) {
-      double currentMin = Double.MAX_VALUE;
+   // TLE
+//   private static double mincostToHireWorkers(int[] quality, int[] wage, int k) {
+//      double currentMin = Double.MAX_VALUE;
+//      for (int i = 0; i < quality.length; i++) { // O(N)
+//         Queue<Double> queue = new PriorityQueue<>(Collections.reverseOrder());
+//         for (int j = 0; j < quality.length; j++) { // O(N)
+//            double potentialWage = (double) wage[i] * ((double) quality[j] / (double) quality[i]);
+//            if (potentialWage >= wage[j]) { // O(logK)
+//               queue.offer(potentialWage);
+//            } else {
+//               queue.offer(100000d);
+//            }
+//            while (queue.size() > k) {
+//               queue.poll(); // O(logK)
+//            }
+//         }
+//         double min = 0;
+//         while (!queue.isEmpty()) {
+//            min += queue.poll();
+//         }
+//         currentMin = Math.min(currentMin, min);
+//      }
+//      return currentMin;
+//   }
 
-      double[] tmpWage = new double[wage.length];
-      for (int i = 0; i < quality.length; i++) {
-         for (int j = 0; j < quality.length; j++) {
-            if (i == j) {
-               tmpWage[j] = wage[i];
-            } else {
-               double potentialWage = (double) wage[i] * ((double) quality[j] / (double) quality[i]);
-               if (potentialWage >= wage[j]) {
-                  tmpWage[j] = potentialWage;
-               } else {
-                  tmpWage[j] = 1000000;
-               }
-            }
-         }
-         Arrays.sort(tmpWage);
-         double min = 0;
-         for (int l = 0; l < k; l++) {
-            min += tmpWage[l];
-         }
-         currentMin = Math.min(currentMin, min);
+   private static double mincostToHireWorkers(int[] quality, int[] wage, int k) {
+      int N = quality.length;
+      Worker[] workers = new Worker[N];
+      for (int i = 0; i < N; ++i) {
+         workers[i] = new Worker(quality[i], wage[i]);
       }
-      return currentMin;
+      Arrays.sort(workers);
+
+      double ans = Double.MAX_VALUE;
+      Queue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+      int ratioSum = 0;
+      for (Worker worker : workers) {
+         queue.offer(worker.quality);
+         ratioSum += worker.quality;
+         while (queue.size() > k) {
+            ratioSum -= queue.poll();
+         }
+         if (queue.size() == k) {
+            ans = Math.min(ans, ratioSum * worker.ratio());
+         }
+      }
+      return ans;
+   }
+}
+
+class Worker implements Comparable<Worker> {
+
+   public int quality, wage;
+
+   public Worker(int q, int w) {
+      quality = q;
+      wage = w;
+   }
+
+   public double ratio() {
+      return (double) wage / quality;
+   }
+
+   public int compareTo(Worker other) {
+      return Double.compare(ratio(), other.ratio());
    }
 }
