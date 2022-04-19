363eba10b1d172e12c6e1d2a7197585455728ab2
ganzhab <bodaganj@gmail.com>
2022-04-19 12:18:26 +0200

    easy Strings are implemented

--- src/main/java/com/coding/tasks/leetcode/second_round/top_interview_questions/easy/string/LongestCommonPrefix.java
+++ src/main/java/com/coding/tasks/leetcode/second_round/top_interview_questions/easy/string/LongestCommonPrefix.java
@@ -3,13 +3,32 @@ package com.coding.tasks.leetcode.second_round.top_interview_questions.easy.stri
 public class LongestCommonPrefix {
 
    public static void main(String[] args) {
-//      String[] array1 = new String[]{"ab", "a"};
-//      System.out.println(longestCommonPrefix(array1));
-//
-//      String[] array2 = new String[]{"dog", "racecar", "car"};
-//      System.out.println(longestCommonPrefix(array2));
-//   }
-//
-//   private static String longestCommonPrefix(String[] strs) {
+      String[] array1 = new String[]{"ab", "a"};
+      System.out.println(longestCommonPrefix(array1));
+
+      String[] array2 = new String[]{"dog", "racecar", "car"};
+      System.out.println(longestCommonPrefix(array2));
+   }
+
+   private static String longestCommonPrefix(String[] strs) {
+      int length = Integer.MAX_VALUE;
+      for (String str : strs) {
+         length = Math.min(length, str.length());
+      }
+
+      StringBuilder longest = new StringBuilder();
+      int counter = 0;
+      while (counter < length) {
+         char expected = strs[0].charAt(counter);
+         for (String str : strs) {
+            if (str.charAt(counter) != expected) {
+               return longest.toString();
+            }
+         }
+         longest.append(expected);
+         counter++;
+      }
+
+      return longest.toString();
    }
 }
--- src/main/java/com/coding/tasks/leetcode/second_round/top_interview_questions/easy/string/ReverseInteger.java
+++ src/main/java/com/coding/tasks/leetcode/second_round/top_interview_questions/easy/string/ReverseInteger.java
@@ -3,12 +3,34 @@ package com.coding.tasks.leetcode.second_round.top_interview_questions.easy.stri
 public class ReverseInteger {
 
    public static void main(String[] args) {
-//      System.out.println(reverse(234));
-//      System.out.println(reverse(-20));
-//      System.out.println(reverse(0));
-//      System.out.println(reverse(1534236469));
-//   }
-//
-//   private static int reverse(int x) {
+      System.out.println(reverse(234));
+      System.out.println(reverse(-20));
+      System.out.println(reverse(0));
+      System.out.println(reverse(1534236469));
+   }
+
+   private static int reverse(int x) {
+      StringBuilder sb = new StringBuilder();
+      if (x < 0) {
+         String xstr = x + "";
+         int counter = 1;
+         while (counter < xstr.length()) {
+            sb.append(xstr.charAt(counter));
+            counter++;
+         }
+         String finalStr = sb.reverse().toString();
+         try {
+            return Integer.parseInt(finalStr) * -1;
+         } catch (NumberFormatException exception) {
+            return 0;
+         }
+      } else {
+         String finalStr = sb.append(x + "").reverse().toString();
+         try {
+            return Integer.parseInt(finalStr);
+         } catch (NumberFormatException exception) {
+            return 0;
+         }
+      }
    }
 }
