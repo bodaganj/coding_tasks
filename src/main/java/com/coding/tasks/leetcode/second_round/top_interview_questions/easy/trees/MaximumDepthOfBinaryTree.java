ab5e1fa10f787e3b979d651473d0d36b1d9ea7bf
ganzhab <bodaganj@gmail.com>
2022-04-21 21:40:10 +0200

    MaximumDepthOfBinaryTree is implemented recursively

--- src/main/java/com/coding/tasks/leetcode/second_round/top_interview_questions/easy/trees/MaximumDepthOfBinaryTree.java
+++ src/main/java/com/coding/tasks/leetcode/second_round/top_interview_questions/easy/trees/MaximumDepthOfBinaryTree.java
@@ -2,6 +2,8 @@ package com.coding.tasks.leetcode.second_round.top_interview_questions.easy.tree
 
 public class MaximumDepthOfBinaryTree {
 
+   private static int max = 0;
+
    public static void main(String[] args) {
       TreeNode tn7 = new TreeNode(7);
       TreeNode tn15 = new TreeNode(15);
@@ -9,24 +11,38 @@ public class MaximumDepthOfBinaryTree {
       TreeNode tn9 = new TreeNode(9);
       TreeNode tn3 = new TreeNode(3, tn9, tn20);
 
-//      System.out.println(maxDepth(tn3));
-//
-//      TreeNode tn5 = new TreeNode(5);
-//      TreeNode tn1 = new TreeNode(1);
-//      TreeNode tn6 = new TreeNode(6);
-//      TreeNode tn8 = new TreeNode(8);
-//      TreeNode tn11 = new TreeNode(1, tn5, tn1);
-//      TreeNode tn33 = new TreeNode(3, null, tn6);
-//      TreeNode tn_1 = new TreeNode(-1, null, tn8);
-//      TreeNode tn2 = new TreeNode(2, tn11, null);
-//      TreeNode tn4 = new TreeNode(4, tn33, tn_1);
-//      TreeNode tn0 = new TreeNode(0, tn2, tn4);
-//
-//      System.out.println(maxDepth(tn0));
-//   }
-//
-//   // recursion
-//   private static int maxDepth(TreeNode root) {
+      System.out.println(maxDepth(tn3));
+
+      TreeNode tn5 = new TreeNode(5);
+      TreeNode tn1 = new TreeNode(1);
+      TreeNode tn6 = new TreeNode(6);
+      TreeNode tn8 = new TreeNode(8);
+      TreeNode tn11 = new TreeNode(1, tn5, tn1);
+      TreeNode tn33 = new TreeNode(3, null, tn6);
+      TreeNode tn_1 = new TreeNode(-1, null, tn8);
+      TreeNode tn2 = new TreeNode(2, tn11, null);
+      TreeNode tn4 = new TreeNode(4, tn33, tn_1);
+      TreeNode tn0 = new TreeNode(0, tn2, tn4);
+
+      System.out.println(maxDepth(tn0));
+   }
+
+   private static int maxDepth(TreeNode root) {
+      if (root == null) {
+         return 0;
+      }
+
+      max = 0;
+      getMax(root, 0);
+      return max;
+   }
+
+   private static void getMax(TreeNode node, int current) {
+      if (node != null) {
+         max = Math.max(current + 1, max);
+         getMax(node.left, current + 1);
+         getMax(node.right, current + 1);
+      }
    }
 
    static class TreeNode {
