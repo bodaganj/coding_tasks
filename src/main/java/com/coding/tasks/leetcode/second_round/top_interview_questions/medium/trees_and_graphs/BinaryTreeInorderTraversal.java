97a19cb768091104d34568aec202a3e7de2c34cf
ganzhab <bodaganj@gmail.com>
2022-04-22 18:15:51 +0200

    two medium tasks are implemented

--- src/main/java/com/coding/tasks/leetcode/second_round/top_interview_questions/medium/trees_and_graphs/BinaryTreeInorderTraversal.java
+++ src/main/java/com/coding/tasks/leetcode/second_round/top_interview_questions/medium/trees_and_graphs/BinaryTreeInorderTraversal.java
@@ -1,16 +1,61 @@
 package com.coding.tasks.leetcode.second_round.top_interview_questions.medium.trees_and_graphs;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.Stack;
+
 public class BinaryTreeInorderTraversal {
 
    public static void main(String[] args) {
       TreeNode t3 = new TreeNode(3);
       TreeNode t2 = new TreeNode(2, t3, null);
       TreeNode t1 = new TreeNode(1, null, t2);
-//      System.out.println(inorderTraversalRec(t1));
-//      System.out.println(inorderTraversalIter(t1));
-//   }
-//
-//   private static List<Integer> inorderTraversalRec(TreeNode root) {
+      System.out.println(inorderTraversalRec(t1));
+      System.out.println(inorderTraversalIter(t1));
+   }
+
+   private static List<Integer> inorderTraversalRec(TreeNode root) {
+      if (root == null) {
+         return Collections.emptyList();
+      } else {
+         List<Integer> ans = new ArrayList<>();
+         recursion(root, ans);
+         return ans;
+      }
+   }
+
+   private static void recursion(TreeNode root, List<Integer> ans) {
+      if (root != null) {
+         recursion(root.left, ans);
+         ans.add(root.val);
+         recursion(root.right, ans);
+      }
+   }
+
+   private static List<Integer> inorderTraversalIter(TreeNode root) {
+      if (root == null) {
+         return Collections.emptyList();
+      }
+
+      Set<TreeNode> visited = new HashSet<>();
+      List<Integer> ans = new ArrayList<>();
+      Stack<TreeNode> stack = new Stack<>();
+      stack.add(root);
+      while (!stack.isEmpty()) {
+         while (stack.peek().left != null && !visited.contains(stack.peek().left)) {
+            stack.add(stack.peek().left);
+         }
+         TreeNode pop = stack.pop();
+         visited.add(pop);
+         ans.add(pop.val);
+         if (pop.right != null && !visited.contains(pop.right)) {
+            stack.add(pop.right);
+         }
+      }
+      return ans;
    }
 
    static class TreeNode {
--- src/main/java/com/coding/tasks/leetcode/second_round/top_interview_questions/medium/trees_and_graphs/BinaryTreeZigzagLevelOrderTraversal.java
+++ src/main/java/com/coding/tasks/leetcode/second_round/top_interview_questions/medium/trees_and_graphs/BinaryTreeZigzagLevelOrderTraversal.java
@@ -1,5 +1,11 @@
 package com.coding.tasks.leetcode.second_round.top_interview_questions.medium.trees_and_graphs;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class BinaryTreeZigzagLevelOrderTraversal {
 
    public static void main(String[] args) {
@@ -7,11 +13,41 @@ public class BinaryTreeZigzagLevelOrderTraversal {
       TreeNode t15 = new TreeNode(15);
       TreeNode t20 = new TreeNode(20, t15, t7);
       TreeNode t9 = new TreeNode(9);
-//      TreeNode t3 = new TreeNode(3, t9, t20);
-//      System.out.println(zigzagLevelOrder(t3));
-//   }
-//
-//   private static List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+      TreeNode t3 = new TreeNode(3, t9, t20);
+      System.out.println(zigzagLevelOrder(t3));
+   }
+
+   private static List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+      List<List<Integer>> ans = new ArrayList<>();
+      if (root != null) {
+         boolean zigZagFlag = false;
+         Queue<TreeNode> queue = new LinkedList<>();
+         queue.offer(root);
+
+         while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> tmp = new ArrayList<>();
+            while (size > 0) {
+               size--;
+               TreeNode poll = queue.poll();
+               tmp.add(poll.val);
+
+               if (poll.left != null) {
+                  queue.offer(poll.left);
+               }
+               if (poll.right != null) {
+                  queue.offer(poll.right);
+               }
+            }
+
+            if (zigZagFlag) {
+               Collections.reverse(tmp);
+            }
+            ans.add(tmp);
+            zigZagFlag = !zigZagFlag;
+         }
+      }
+      return ans;
    }
 
    static class TreeNode {
