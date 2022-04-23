eaea802b57d10cf9507c1ebf40df669cec2c65e0
ganzhab <bodaganj@gmail.com>
2022-04-23 17:48:09 +0200

    medium tasks are finished

--- src/main/java/com/coding/tasks/leetcode/second_round/top_interview_questions/medium/trees_and_graphs/NumberOfIslands.java
+++ src/main/java/com/coding/tasks/leetcode/second_round/top_interview_questions/medium/trees_and_graphs/NumberOfIslands.java
@@ -2,6 +2,8 @@ package com.coding.tasks.leetcode.second_round.top_interview_questions.medium.tr
 
 public class NumberOfIslands {
 
+   private static int[][] dir = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
    public static void main(String[] args) {
       char[][] arr = new char[][]{
          {'1', '1', '0', '0', '0'},
@@ -10,9 +12,28 @@ public class NumberOfIslands {
          {'0', '0', '0', '1', '1'}
       };
 
-//      System.out.println(numIslands(arr));
-//   }
-//
-//   private static int numIslands(char[][] grid) {
+      System.out.println(numIslands(arr));
+   }
+
+   private static int numIslands(char[][] grid) {
+      int counter = 0;
+      for (int i = 0; i < grid.length; i++) {
+         for (int j = 0; j < grid[i].length; j++) {
+            if (grid[i][j] == '1') {
+               counter++;
+               dfs(grid, i, j);
+            }
+         }
+      }
+      return counter;
+   }
+
+   private static void dfs(char[][] grid, int i, int j) {
+      if (i >= 0 && i < grid.length && j >= 0 && j < grid[i].length && grid[i][j] == '1') {
+         grid[i][j] = '2';
+         for (int[] ints : dir) {
+            dfs(grid, i + ints[0], j + ints[1]);
+         }
+      }
    }
 }
