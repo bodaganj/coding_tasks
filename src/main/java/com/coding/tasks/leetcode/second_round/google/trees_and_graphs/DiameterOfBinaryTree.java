77c40e39b552c1384182cb325836aec0182bb743
ganzhab <bodaganj@gmail.com>
2022-04-25 17:52:17 +0200

    DiameterOfBinaryTree is implemented both ways

--- src/main/java/com/coding/tasks/leetcode/second_round/google/trees_and_graphs/DiameterOfBinaryTree.java
+++ src/main/java/com/coding/tasks/leetcode/second_round/google/trees_and_graphs/DiameterOfBinaryTree.java
@@ -1,8 +1,13 @@
 package com.coding.tasks.leetcode.second_round.google.trees_and_graphs;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class DiameterOfBinaryTree {
 
    private static int maxPath = 0;
+   private static List<TreeNode> maxPathNodes;
 
    public static void main(String[] args) {
       TreeNode tn5 = new TreeNode(5);
@@ -12,23 +17,10 @@ public class DiameterOfBinaryTree {
       TreeNode tn1 = new TreeNode(1, tn2, tn3);
 
       System.out.println(diameterOfBinaryTree(tn1));
-
-      TreeNode t14 = new TreeNode(14);
-      TreeNode t13 = new TreeNode(13, null, t14);
-      TreeNode t12 = new TreeNode(12);
-      TreeNode t11 = new TreeNode(11, null, t13);
-      TreeNode t10 = new TreeNode(10);
-      TreeNode t9 = new TreeNode(9, null, t10);
-      TreeNode t8 = new TreeNode(8);
-      TreeNode t7 = new TreeNode(7);
-      TreeNode t6 = new TreeNode(6, t8, t9);
-      TreeNode t5 = new TreeNode(5, t6, t7);
-      TreeNode t4 = new TreeNode(4);
-      TreeNode t3 = new TreeNode(3, t4, t5);
-      TreeNode t2 = new TreeNode(2, t12, t11);
-      TreeNode t1 = new TreeNode(1, t2, t3);
-
-//      System.out.println(diameterOfBinaryTree(t1));
+      System.out.println("==========");
+      for (TreeNode maxPathNode : maxPathNodes) {
+         System.out.println(maxPathNode.val);
+      }
    }
 
    private static int diameterOfBinaryTree(TreeNode root) {
@@ -37,14 +29,30 @@ public class DiameterOfBinaryTree {
       return maxPath;
    }
 
-   private static int getMaxPath(TreeNode node) {
+   private static List<TreeNode> getMaxPath(TreeNode node) {
       if (node == null) {
-         return -1;
+         return Collections.emptyList();
       } else {
-         int maxLeft = 1 + getMaxPath(node.left);
-         int maxRight = 1 + getMaxPath(node.right);
-         maxPath = Math.max(maxPath, maxLeft + maxRight);
-         return Math.max(maxLeft, maxRight);
+         List<TreeNode> leftList = getMaxPath(node.left);
+         List<TreeNode> rightList = getMaxPath(node.right);
+         List<TreeNode> currentList = new ArrayList<>();
+         currentList.addAll(leftList);
+         currentList.addAll(rightList);
+         currentList.add(node);
+
+         if (maxPath < currentList.size() - 1) {
+            maxPath = currentList.size() - 1;
+            maxPathNodes = currentList;
+         }
+
+         List<TreeNode> ret = new ArrayList<>();
+         if (leftList.size() > rightList.size()) {
+            ret.addAll(leftList);
+         } else {
+            ret.addAll(rightList);
+         }
+         ret.add(node);
+         return ret;
       }
    }
 
