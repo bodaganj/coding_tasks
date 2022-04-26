7a83da9720741653636f58d278ba55dfaf5c0514
ganzhab <bodaganj@gmail.com>
2022-04-26 17:21:50 +0200

    SymmetricTree is implemented

--- src/main/java/com/coding/tasks/leetcode/second_round/amazon/trees_and_graphs/SymmetricTree.java
+++ src/main/java/com/coding/tasks/leetcode/second_round/amazon/trees_and_graphs/SymmetricTree.java
@@ -1,13 +1,70 @@
 package com.coding.tasks.leetcode.second_round.amazon.trees_and_graphs;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class SymmetricTree {
 
    public static void main(String[] args) {
 
    }
 
-//   private static boolean isSymmetricIteratively(TreeNode root) {
-//   }
+   private static boolean isSymmetricRec(TreeNode root) {
+      return rec(root.left, root.right);
+   }
+
+   private static boolean rec(TreeNode left, TreeNode right) {
+      if (left == right) {
+         return true;
+      } else if (left == null || right == null) {
+         return false;
+      } else if (left.val != right.val) {
+         return false;
+      }
+
+      return rec(left.left, right.right) && rec(left.right, right.left);
+   }
+
+   private static boolean isSymmetricIteratively(TreeNode root) {
+      Queue<TreeNode> queueLeft = new LinkedList<>();
+      Queue<TreeNode> queueRight = new LinkedList<>();
+      if (root.left == root.right) {
+         return true;
+      } else if (root.left == null || root.right == null) {
+         return false;
+      }
+      queueLeft.offer(root.left);
+      queueRight.offer(root.right);
+
+      while (!queueLeft.isEmpty() && !queueRight.isEmpty()) {
+         TreeNode pollLeft = queueLeft.poll();
+         TreeNode pollRight = queueRight.poll();
+
+         if (pollLeft.val != pollRight.val) {
+            return false;
+         }
+
+         if (pollLeft.left != pollRight.right) {
+            if (pollLeft.left == null || pollRight.right == null) {
+               return false;
+            } else {
+               queueLeft.offer(pollLeft.left);
+               queueRight.offer(pollRight.right);
+            }
+         }
+
+         if (pollLeft.right != pollRight.left) {
+            if (pollLeft.right == null || pollRight.left == null) {
+               return false;
+            } else {
+               queueLeft.offer(pollLeft.right);
+               queueRight.offer(pollRight.left);
+            }
+         }
+      }
+
+      return queueLeft.isEmpty() && queueRight.isEmpty();
+   }
 
    static class TreeNode {
 
--- src/main/java/com/coding/tasks/leetcode/second_round/amazon/trees_and_graphs/ValidateBinarySearchTree.java
+++ src/main/java/com/coding/tasks/leetcode/second_round/amazon/trees_and_graphs/ValidateBinarySearchTree.java
@@ -1,24 +1,42 @@
 package com.coding.tasks.leetcode.second_round.amazon.trees_and_graphs;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ValidateBinarySearchTree {
 
    public static void main(String[] args) {
-//      TreeNode tn0 = new TreeNode(0);
-//      TreeNode tn3 = new TreeNode(3);
-////      TreeNode tn5 = new TreeNode(5, null, tn0);
-//      TreeNode tn5 = new TreeNode(5);
-//      TreeNode tn4 = new TreeNode(4, tn3, tn5);
-//      TreeNode tn1 = new TreeNode(1);
-//      TreeNode tn2 = new TreeNode(2, tn1, tn4);
-//
-//      System.out.println(isValidBST(tn2));
-//
+      TreeNode tn6 = new TreeNode(6);
+      TreeNode tn3 = new TreeNode(3);
+      TreeNode tn4 = new TreeNode(4, tn3, tn6);
+      TreeNode tn1 = new TreeNode(1);
+      TreeNode tn5 = new TreeNode(5, tn1, tn4);
+
+      System.out.println(isValidBST(tn5));
+
 //      TreeNode tn111 = new TreeNode(-2147483648);
 //
 //      System.out.println(isValidBST(tn111));
-//   }
-//
-//   private static boolean isValidBST(TreeNode root) {
+   }
+
+   private static boolean isValidBST(TreeNode root) {
+      List<Integer> inOrderTraversal = new ArrayList<>();
+      dfs(root, inOrderTraversal);
+      for (int i = 1; i < inOrderTraversal.size(); i++) {
+         if (inOrderTraversal.get(i - 1) >= inOrderTraversal.get(i)) {
+            return false;
+         }
+      }
+
+      return true;
+   }
+
+   private static void dfs(TreeNode root, List<Integer> inOrderTraversal) {
+      if (root != null) {
+         dfs(root.left, inOrderTraversal);
+         inOrderTraversal.add(root.val);
+         dfs(root.right, inOrderTraversal);
+      }
    }
 
    static class TreeNode {
