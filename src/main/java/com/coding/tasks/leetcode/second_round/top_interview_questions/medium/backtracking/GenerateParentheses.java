4881f2f80de878b57d8b785267001effcd1271c0
ganzhab <bodaganj@gmail.com>
2022-04-27 17:36:15 +0200

    two simple backtracking tasks were implemented

--- src/main/java/com/coding/tasks/leetcode/second_round/top_interview_questions/medium/backtracking/GenerateParentheses.java
+++ src/main/java/com/coding/tasks/leetcode/second_round/top_interview_questions/medium/backtracking/GenerateParentheses.java
@@ -1,12 +1,31 @@
 package com.coding.tasks.leetcode.second_round.top_interview_questions.medium.backtracking;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GenerateParentheses {
 
    public static void main(String[] args) {
-//      System.out.println(generateParenthesis(2));
-//      System.out.println(generateParenthesis(3));
-//   }
-//
-//   private static List<String> generateParenthesis(int n) {
+      System.out.println(generateParenthesis(2));
+      System.out.println(generateParenthesis(3));
+   }
+
+   private static List<String> generateParenthesis(int n) {
+      List<String> ans = new ArrayList<>();
+      rec(0, 0, "", n, ans);
+      return ans;
+   }
+
+   private static void rec(int open, int close, String current, int n, List<String> ans) {
+      if (close == n) {
+         ans.add(current);
+         return;
+      }
+      if (open < n) {
+         rec(open + 1, close, current + "(", n, ans);
+      }
+      if (close < open) {
+         rec(open, close + 1, current + ")", n, ans);
+      }
    }
 }
--- src/main/java/com/coding/tasks/leetcode/second_round/top_interview_questions/medium/backtracking/LetterCombinationsOfPhoneNumber.java
+++ src/main/java/com/coding/tasks/leetcode/second_round/top_interview_questions/medium/backtracking/LetterCombinationsOfPhoneNumber.java
@@ -1,5 +1,7 @@
 package com.coding.tasks.leetcode.second_round.top_interview_questions.medium.backtracking;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
@@ -17,10 +19,28 @@ public class LetterCombinationsOfPhoneNumber {
    );
 
    public static void main(String[] args) {
-//      System.out.println(letterCombinations("29"));
-//      System.out.println(letterCombinations("356"));
-//   }
-//
-//   private static List<String> letterCombinations(String digits) {
+      System.out.println(letterCombinations("29"));
+      System.out.println(letterCombinations("356"));
+   }
+
+   private static List<String> letterCombinations(String digits) {
+      if (digits.isEmpty()) {
+         return Collections.emptyList();
+      } else {
+         List<String> ans = new ArrayList<>();
+         rec(digits, "", ans);
+         return ans;
+      }
+   }
+
+   private static void rec(String digit, String current, List<String> ans) {
+      if (digit.isEmpty()) {
+         ans.add(current);
+         return;
+      }
+
+      for (String s : mapping.get(digit.charAt(0))) {
+         rec(digit.substring(1), current + s, ans);
+      }
    }
 }
