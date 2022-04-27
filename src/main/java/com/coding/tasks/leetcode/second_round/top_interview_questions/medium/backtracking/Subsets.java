8696d3a0ad373683cdce99c96f9693580fe58e9a
ganzhab <bodaganj@gmail.com>
2022-04-28 00:05:53 +0200

    medium recursion tasks were implemented

--- src/main/java/com/coding/tasks/leetcode/second_round/top_interview_questions/medium/backtracking/Subsets.java
+++ src/main/java/com/coding/tasks/leetcode/second_round/top_interview_questions/medium/backtracking/Subsets.java
@@ -1,11 +1,30 @@
 package com.coding.tasks.leetcode.second_round.top_interview_questions.medium.backtracking;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Subsets {
 
    public static void main(String[] args) {
-//      System.out.println(subsets(new int[]{1, 2, 3}));
-//   }
-//
-//   private static List<List<Integer>> subsets(int[] nums) {
+      System.out.println(subsets(new int[]{1, 2, 3}));
+   }
+
+   private static List<List<Integer>> subsets(int[] nums) {
+      List<List<Integer>> ans = new ArrayList<>();
+      ans.add(new ArrayList<>());
+
+      for (int num : nums) {
+         int i = 0;
+         List<List<Integer>> newSubsets = new ArrayList<>();
+         while (i < ans.size()) {
+            List<Integer> tmp = new ArrayList<>(ans.get(i));
+            tmp.add(num);
+            newSubsets.add(tmp);
+            i++;
+         }
+         ans.addAll(newSubsets);
+      }
+
+      return ans;
    }
 }
--- src/main/java/com/coding/tasks/leetcode/second_round/top_interview_questions/medium/backtracking/WordSearch.java
+++ src/main/java/com/coding/tasks/leetcode/second_round/top_interview_questions/medium/backtracking/WordSearch.java
@@ -1,28 +1,80 @@
 package com.coding.tasks.leetcode.second_round.top_interview_questions.medium.backtracking;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class WordSearch {
 
-   public static void main(String[] args) {
-//      System.out.println(exist(new char[][]{
-//         {'A', 'B', 'C', 'E'},
-//         {'S', 'F', 'C', 'S'},
-//         {'A', 'D', 'E', 'E'}
-//      }, "ABCCED"));
-//
-//      System.out.println(exist(new char[][]{
-//         {'A', 'B', 'C', 'E'},
-//         {'S', 'F', 'C', 'S'},
-//         {'A', 'D', 'E', 'E'}
-//      }, "ABCB"));
-//
-//      System.out.println(exist(new char[][]{
-//         {'C', 'A', 'A'},
-//         {'A', 'A', 'A'},
-//         {'B', 'C', 'D'}
-//      }, "AAB"));
-//   }
-//
-//   private static boolean exist(char[][] board, String word) {
+   private static int[][] dirs = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
+   public static void main(String[] args) {
+      System.out.println(exist(new char[][]{
+         {'A', 'B', 'C', 'E'},
+         {'S', 'F', 'C', 'S'},
+         {'A', 'D', 'E', 'E'}
+      }, "ABCCED"));
+
+      System.out.println(exist(new char[][]{
+         {'A', 'B', 'C', 'E'},
+         {'S', 'F', 'C', 'S'},
+         {'A', 'D', 'E', 'E'}
+      }, "ABCB"));
+
+      System.out.println(exist(new char[][]{
+         {'C', 'A', 'A'},
+         {'A', 'A', 'A'},
+         {'B', 'C', 'D'}
+      }, "AAB"));
+   }
+
+   private static boolean exist(char[][] board, String word) {
+      char firstChar = word.charAt(0);
+      List<List<Integer>> listOfFirstChar = new ArrayList<>();
+      for (int i = 0; i < board.length; i++) {
+         for (int j = 0; j < board[0].length; j++) {
+            if (board[i][j] == firstChar) {
+               listOfFirstChar.add(List.of(i, j));
+            }
+         }
+      }
+
+      for (List<Integer> coordinates : listOfFirstChar) {
+         Set<List<Integer>> visited = new HashSet<>();
+         visited.add(coordinates);
+         if (dfs(coordinates, board, word, 1, visited)) {
+            return true;
+         }
+      }
+      return false;
+   }
+
+   private static boolean dfs(List<Integer> coordinates, char[][] board, String word, int index, Set<List<Integer>> visitedCoordinates) {
+      if (index == word.length()) {
+         return true;
+      } else {
+         Integer currX = coordinates.get(0);
+         Integer currY = coordinates.get(1);
+
+         for (int[] dir : dirs) {
+            int i = currX + dir[0];
+            int j = currY + dir[1];
+
+            List<Integer> newCoordinates = List.of(i, j);
+            if (i >= 0 && i < board.length && j >= 0 && j < board[0].length
+               && board[i][j] == word.charAt(index) && !visitedCoordinates.contains(newCoordinates)) {
+               visitedCoordinates.add(newCoordinates);
+
+               if (dfs(newCoordinates, board, word, index + 1, visitedCoordinates)) {
+                  return true;
+               }
+
+               visitedCoordinates.remove(newCoordinates);
+            }
+         }
+
+         return false;
+      }
    }
 }
