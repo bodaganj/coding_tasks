853e81f4ce3d6edbd7eaa7fce1ea7b5f2f30fc73
ganzhab <bodaganj@gmail.com>
2022-05-01 22:37:52 +0200

    FirstBadVersion is implemented

--- src/main/java/com/coding/tasks/leetcode/second_round/top_interview_questions/easy/sorting_and_searching/FirstBadVersion.java
+++ src/main/java/com/coding/tasks/leetcode/second_round/top_interview_questions/easy/sorting_and_searching/FirstBadVersion.java
@@ -2,6 +2,22 @@ package com.coding.tasks.leetcode.second_round.top_interview_questions.easy.sort
 
 public class FirstBadVersion {
 
-//   private static int firstBadVersion(int n) {
-//   }
+   private static int firstBadVersion(int n) {
+      int left = 1;
+      int right = n;
+
+      while (left < right) {
+         int mid = left + (right - left) / 2;
+         if (isBadVersion(mid)) {
+            right = mid;
+         } else {
+            left = mid + 1;
+         }
+      }
+      return left;
+   }
+
+   private static boolean isBadVersion(int version) {
+      return true;
+   }
 }
