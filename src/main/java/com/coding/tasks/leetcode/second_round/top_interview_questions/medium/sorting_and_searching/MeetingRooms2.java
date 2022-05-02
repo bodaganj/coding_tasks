def6ed0fad3a40275409e4160a415502acf7488a
ganzhab <bodaganj@gmail.com>
2022-05-02 15:57:11 +0200

    MeetingRooms2 is implemented

--- src/main/java/com/coding/tasks/leetcode/second_round/top_interview_questions/medium/sorting_and_searching/MeetingRooms2.java
+++ src/main/java/com/coding/tasks/leetcode/second_round/top_interview_questions/medium/sorting_and_searching/MeetingRooms2.java
@@ -1,12 +1,35 @@
 package com.coding.tasks.leetcode.second_round.top_interview_questions.medium.sorting_and_searching;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 public class MeetingRooms2 {
 
    public static void main(String[] args) {
-//      System.out.println(minMeetingRooms(new int[][]{{0, 30}, {5, 10}, {15, 20}}));
-//      System.out.println(minMeetingRooms(new int[][]{{2, 4}, {7, 10}}));
-//   }
-//
-//   private static int minMeetingRooms(int[][] intervals) {
+      System.out.println(minMeetingRooms(new int[][]{{0, 30}, {5, 10}, {15, 20}}));
+      System.out.println(minMeetingRooms(new int[][]{{2, 4}, {7, 10}}));
+   }
+
+   private static int minMeetingRooms(int[][] intervals) {
+      Arrays.sort(intervals, Comparator.comparingInt(a -> a[0]));
+
+      int max = 1;
+      Queue<Integer> queue = new PriorityQueue<>();
+      for (int[] interval : intervals) {
+         if (queue.isEmpty()) {
+            queue.offer(interval[1]);
+         } else {
+            if (queue.peek() <= interval[0]) {
+               queue.poll();
+               queue.offer(interval[1]);
+            } else {
+               queue.offer(interval[1]);
+               max = Math.max(max, queue.size());
+            }
+         }
+      }
+      return max;
    }
 }
