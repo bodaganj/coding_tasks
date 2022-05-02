4e691c37c26c85502d8b50fce63501bfc1f919c9
ganzhab <bodaganj@gmail.com>
2022-05-02 15:28:17 +0200

    MergeIntervals and SearchForRange tasks were implemented

--- src/main/java/com/coding/tasks/leetcode/second_round/top_interview_questions/medium/sorting_and_searching/MergeIntervals.java
+++ src/main/java/com/coding/tasks/leetcode/second_round/top_interview_questions/medium/sorting_and_searching/MergeIntervals.java
@@ -1,12 +1,34 @@
 package com.coding.tasks.leetcode.second_round.top_interview_questions.medium.sorting_and_searching;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
 public class MergeIntervals {
 
    public static void main(String[] args) {
-//      System.out.println(Arrays.deepToString(merge(new int[][]{{1, 3}, {2, 6}, {8, 10}, {15, 18}})));
-//      System.out.println(Arrays.deepToString(merge(new int[][]{{1, 4}, {4, 5}})));
-//   }
-//
-//   private static int[][] merge(int[][] intervals) {
+      System.out.println(Arrays.deepToString(merge(new int[][]{{1, 3}, {2, 6}, {8, 10}, {15, 18}})));
+      System.out.println(Arrays.deepToString(merge(new int[][]{{1, 4}, {4, 5}})));
+      System.out.println(Arrays.deepToString(merge(new int[][]{{2, 3}, {2, 2}, {3, 3}, {1, 3}, {5, 7}, {2, 2}, {4, 6}})));
+   }
+
+   private static int[][] merge(int[][] intervals) {
+      Arrays.sort(intervals, Comparator.comparingInt(a -> a[0]));
+
+      List<int[]> ans = new ArrayList<>();
+      for (int[] interval : intervals) {
+         if (!ans.isEmpty() && ans.get(ans.size() - 1)[1] >= interval[0]) {
+            ans.get(ans.size() - 1)[1] = Math.max(ans.get(ans.size() - 1)[1], interval[1]);
+         } else {
+            ans.add(interval);
+         }
+      }
+
+      int[][] result = new int[ans.size()][2];
+      for (int i = 0; i < ans.size(); i++) {
+         result[i] = ans.get(i);
+      }
+      return result;
    }
 }
--- src/main/java/com/coding/tasks/leetcode/second_round/top_interview_questions/medium/sorting_and_searching/SearchForRange.java
+++ src/main/java/com/coding/tasks/leetcode/second_round/top_interview_questions/medium/sorting_and_searching/SearchForRange.java
@@ -1,14 +1,62 @@
 package com.coding.tasks.leetcode.second_round.top_interview_questions.medium.sorting_and_searching;
 
+import java.util.Arrays;
+
 public class SearchForRange {
 
    public static void main(String[] args) {
-//      System.out.println(Arrays.toString(searchRange(new int[]{0, 0, 1, 1, 1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 6, 6, 6, 8, 10, 10}, 4)));
-////      System.out.println(Arrays.toString(searchRange(new int[]{2,2}, 2)));
-////      System.out.println(Arrays.toString(searchRange(new int[]{5, 7, 7, 8, 8, 10}, 8)));
-////      System.out.println(Arrays.toString(searchRange(new int[]{5, 7, 7, 8, 8, 10}, 6)));
-//   }
-//
-//   private static int[] searchRange(int[] nums, int target) {
+      System.out.println(Arrays.toString(searchRange(new int[]{0, 0, 1, 1, 1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 6, 6, 6, 8, 10, 10}, 4)));
+      System.out.println(Arrays.toString(searchRange(new int[]{2, 2}, 2)));
+      System.out.println(Arrays.toString(searchRange(new int[]{5, 7, 7, 8, 8, 10}, 8)));
+      System.out.println(Arrays.toString(searchRange(new int[]{5, 7, 7, 8, 8, 10}, 6)));
+      System.out.println(Arrays.toString(searchRange(new int[]{1}, 1)));
+      System.out.println(Arrays.toString(searchRange(new int[]{0, 0, 0, 1, 2, 3}, 0)));
+   }
+
+   private static int[] searchRange(int[] nums, int target) {
+      int left = 0;
+      int right = nums.length - 1;
+      while (left <= right) {
+         int mid = left + (right - left) / 2;
+         if (nums[mid] == target) {
+            int start = getStart(nums, 0, mid, target);
+            int end = getEnd(nums, mid, right, target);
+            return new int[]{start, end};
+         } else if (nums[mid] > target) {
+            right = mid - 1;
+         } else {
+            left = mid + 1;
+         }
+      }
+
+      return new int[]{-1, -1};
+   }
+
+   private static int getStart(int[] nums, int left, int right, int target) {
+      int ans = right;
+      while (left <= right) {
+         int mid = left + (right - left) / 2;
+         if (nums[mid] == target) {
+            ans = mid;
+            right = mid - 1;
+         } else {
+            left = mid + 1;
+         }
+      }
+      return ans;
+   }
+
+   private static int getEnd(int[] nums, int left, int right, int target) {
+      int ans = left;
+      while (left <= right) {
+         int mid = left + (right - left) / 2;
+         if (nums[mid] == target) {
+            ans = mid;
+            left = mid + 1;
+         } else {
+            right = mid - 1;
+         }
+      }
+      return ans;
    }
 }
