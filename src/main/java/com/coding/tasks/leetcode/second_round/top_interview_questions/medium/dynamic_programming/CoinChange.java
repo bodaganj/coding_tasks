2b75b7d89f843f1cd85ed59e3639a291dd6c6dea
ganzhab <bodaganj@gmail.com>
2022-05-05 21:37:06 +0200

    medium DP problems are implemented

--- src/main/java/com/coding/tasks/leetcode/second_round/top_interview_questions/medium/dynamic_programming/CoinChange.java
+++ src/main/java/com/coding/tasks/leetcode/second_round/top_interview_questions/medium/dynamic_programming/CoinChange.java
@@ -5,37 +5,26 @@ import java.util.Arrays;
 public class CoinChange {
 
    public static void main(String[] args) {
+      System.out.println(coinChange(new int[]{2, 3, 6, 7}, 13));
       System.out.println(coinChange(new int[]{1, 2, 5}, 11));
       System.out.println(coinChange(new int[]{2}, 3));
       System.out.println(coinChange(new int[]{83, 186, 408, 419}, 6249));
    }
 
    private static int coinChange(int[] coins, int amount) {
-      if (amount == 0) {
-         return 0;
-      }
-      Arrays.sort(coins);
-      int[][] dp = new int[coins.length + 1][amount + 1];
+      int[] dp = new int[amount + 1];
+      Arrays.fill(dp, amount + 1);
+      dp[0] = 0;
 
-      for (int i = 1; i < dp.length; i++) {
-         int currentCoin = coins[i - 1];
-         for (int j = 1; j < dp[0].length; j++) {
-            if (j == currentCoin) {
-               dp[i][j] = 1;
-            } else if (j > coins[i - 1]) {
-               if (dp[i - 1][j] != 0 && dp[i][j - coins[i - 1]] != 0) {
-                  dp[i][j] = Math.min(dp[i - 1][j], 1 + dp[i][j - coins[i - 1]]);
-               } else if (dp[i][j - coins[i - 1]] != 0) {
-                  dp[i][j] = 1 + dp[i][j - coins[i - 1]];
-               } else {
-                  dp[i][j] = dp[i - 1][j];
-               }
-            } else {
-               dp[i][j] = dp[i - 1][j];
+      Arrays.sort(coins);
+      for (int coin : coins) {
+         for (int i = 1; i <= amount; i++) {
+            if (i >= coin) {
+               dp[i] = Math.min(dp[i], dp[i - coin] + 1);
             }
          }
       }
 
-      return dp[coins.length][amount] == 0 ? -1 : dp[coins.length][amount];
+      return dp[amount] > amount ? -1 : dp[amount];
    }
 }
--- src/main/java/com/coding/tasks/leetcode/second_round/top_interview_questions/medium/dynamic_programming/LongestIncreasingSubsequence.java
+++ src/main/java/com/coding/tasks/leetcode/second_round/top_interview_questions/medium/dynamic_programming/LongestIncreasingSubsequence.java
@@ -1,12 +1,30 @@
 package com.coding.tasks.leetcode.second_round.top_interview_questions.medium.dynamic_programming;
 
+import java.util.Arrays;
+
 public class LongestIncreasingSubsequence {
 
    public static void main(String[] args) {
-//      System.out.println(lengthOfLIS(new int[]{0, 1, 0, 3, 2, 3}));
-//      System.out.println(lengthOfLIS(new int[]{10, 9, 2, 5, 3, 7, 101, 18}));
-//   }
-//
-//   private static int lengthOfLIS(int[] nums) {
+      System.out.println(lengthOfLIS(new int[]{0, 1, 0, 3, 2, 3}));
+      System.out.println(lengthOfLIS(new int[]{10, 9, 2, 5, 3, 7, 101, 18}));
+   }
+
+   private static int lengthOfLIS(int[] nums) {
+      int[] dp = new int[nums.length];
+      Arrays.fill(dp, 1);
+
+      for (int i = 0; i < nums.length; i++) {
+         for (int j = 0; j < i; j++) {
+            if (nums[i] > nums[j]) {
+               dp[i] = Math.max(dp[i], dp[j] + 1);
+            }
+         }
+      }
+
+      int max = dp[0];
+      for (int i = 1; i < dp.length; i++) {
+         max = Math.max(max, dp[i]);
+      }
+      return max;
    }
 }
