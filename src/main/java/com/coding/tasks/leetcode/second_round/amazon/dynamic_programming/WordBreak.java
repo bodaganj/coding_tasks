bed3618b8429d438aa0a6ce17750783bfc7ac713
ganzhab <bodaganj@gmail.com>
2022-05-07 22:49:54 +0200

    Amazon DP tasks are finished

--- src/main/java/com/coding/tasks/leetcode/second_round/amazon/dynamic_programming/WordBreak.java
+++ src/main/java/com/coding/tasks/leetcode/second_round/amazon/dynamic_programming/WordBreak.java
@@ -1,15 +1,47 @@
 package com.coding.tasks.leetcode.second_round.amazon.dynamic_programming;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 public class WordBreak {
 
    public static void main(String[] args) {
-//      System.out.println(wordBreak("catsandog", List.of("cats", "dog", "sand", "and", "cat")));
-//      System.out.println(wordBreak("ccbb", List.of("bc", "cb")));
-//      System.out.println(wordBreak("aaaaaaa", List.of("aaaa", "aa")));
-//      System.out.println(wordBreak("leetcode", List.of("leet", "code")));
-//      System.out.println(wordBreak("bogdan", List.of("maya", "yana")));
-//   }
-//
-//   private static boolean wordBreak(String s, List<String> wordDict) {
+      System.out.println(wordBreak("catsandog", List.of("cats", "dog", "sand", "and", "cat"))); // false
+      System.out.println(wordBreak("ccbb", List.of("bc", "cb"))); // false
+      System.out.println(wordBreak("aaaaaaa", List.of("aaaa", "aa"))); // false
+      System.out.println(wordBreak("leetcode", List.of("leet", "code"))); // true
+      System.out.println(wordBreak("bogdan", List.of("maya", "yana"))); // false
+   }
+
+   private static boolean wordBreak(String s, List<String> wordDict) {
+      Map<String, Boolean> memo = new HashMap<>();
+      return rec(s, new HashSet<>(wordDict), memo);
+   }
+
+   private static boolean rec(String s, Set<String> set, Map<String, Boolean> memo) {
+      if (s.isEmpty()) {
+         return true;
+      }
+
+      if (memo.containsKey(s)) {
+         return memo.get(s);
+      }
+
+      for (int i = 1; i <= s.length(); i++) {
+         String substring = s.substring(0, i);
+         if (set.contains(substring)) {
+            boolean flag = rec(s.substring(i), set, memo);
+            if (flag) {
+               memo.put(s, true);
+               return true;
+            }
+         }
+      }
+
+      memo.put(s, false);
+      return false;
    }
 }
--- src/main/java/com/coding/tasks/leetcode/second_round/google/dynamic_programming/CoinChange.java
+++ src/main/java/com/coding/tasks/leetcode/second_round/google/dynamic_programming/CoinChange.java
@@ -1,14 +1,29 @@
 package com.coding.tasks.leetcode.second_round.google.dynamic_programming;
 
+import java.util.Arrays;
+
 public class CoinChange {
 
    public static void main(String[] args) {
-//      System.out.println(coinChange(new int[]{1}, 0));
-//      System.out.println(coinChange(new int[]{2}, 3));
-//      System.out.println(coinChange(new int[]{1, 2, 5}, 11));
-//      System.out.println(coinChange(new int[]{186, 419, 83, 408}, 6249));
-//   }
-//
-//   private static int coinChange(int[] coins, int amount) {
+      System.out.println(coinChange(new int[]{1}, 0));
+      System.out.println(coinChange(new int[]{2}, 3));
+      System.out.println(coinChange(new int[]{1, 2, 5}, 11));
+      System.out.println(coinChange(new int[]{186, 419, 83, 408}, 6249));
+   }
+
+   private static int coinChange(int[] coins, int amount) {
+      int[] dp = new int[amount + 1];
+      Arrays.fill(dp, amount + 1);
+      dp[0] = 0;
+
+      for (int i = 1; i < dp.length; i++) {
+         for (int coin : coins) {
+            if (i >= coin) {
+               dp[i] = Math.min(dp[i], 1 + dp[i - coin]);
+            }
+         }
+      }
+
+      return dp[amount] > amount ? -1 : dp[amount];
    }
 }
