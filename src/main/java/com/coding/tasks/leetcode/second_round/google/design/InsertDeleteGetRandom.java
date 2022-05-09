0da792ad6c193b9f83a8ad577c6dfc0cadf1d446
ganzhab <bodaganj@gmail.com>
2022-05-10 00:21:13 +0200

    several google design tasks were implemented

--- src/main/java/com/coding/tasks/leetcode/second_round/google/design/InsertDeleteGetRandom.java
+++ src/main/java/com/coding/tasks/leetcode/second_round/google/design/InsertDeleteGetRandom.java
@@ -1,30 +1,63 @@
 package com.coding.tasks.leetcode.second_round.google.design;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
 public class InsertDeleteGetRandom {
 
    public static void main(String[] args) {
       RandomizedSet o = new RandomizedSet();
-//      System.out.println(o.insert(1)); // true
-//      System.out.println(o.remove(2)); //
-//      System.out.println(o.insert(2));
-//      System.out.println(o.getRandom());
-//      System.out.println(o.remove(1));
-//      System.out.println(o.insert(2));
-//      System.out.println(o.getRandom());
+      System.out.println(o.insert(1)); // true
+      System.out.println(o.remove(2)); //
+      System.out.println(o.insert(2));
+      System.out.println(o.getRandom());
+      System.out.println(o.remove(1));
+      System.out.println(o.insert(2));
+      System.out.println(o.getRandom());
    }
 
    static class RandomizedSet {
 
+      private Map<Integer, Integer> map;
+      private List<Integer> list;
+      private Random rand;
+
       public RandomizedSet() {
+         map = new HashMap<>();
+         list = new ArrayList<>();
+         rand = new Random();
       }
 
-//      public boolean insert(int val) {
-//      }
-//
-//      public boolean remove(int val) {
-//      }
-//
-//      public int getRandom() {
-//      }
+      public boolean insert(int val) {
+         if (map.containsKey(val)) {
+            return false;
+         } else {
+            map.put(val, list.size());
+            list.add(val);
+            return true;
+         }
+      }
+
+      public boolean remove(int val) {
+         if (map.containsKey(val)) {
+            Integer indexToRemove = map.get(val);
+            Integer lastElement = list.get(list.size() - 1);
+            list.set(indexToRemove, lastElement);
+            map.put(lastElement, indexToRemove);
+
+            list.remove(list.size() - 1);
+            map.remove(val);
+            return true;
+         } else {
+            return false;
+         }
+      }
+
+      public int getRandom() {
+         return list.get(rand.nextInt(list.size()));
+      }
    }
 }
--- src/main/java/com/coding/tasks/leetcode/second_round/google/design/MinStackTask.java
+++ src/main/java/com/coding/tasks/leetcode/second_round/google/design/MinStackTask.java
@@ -1,5 +1,7 @@
 package com.coding.tasks.leetcode.second_round.google.design;
 
+import java.util.Stack;
+
 public class MinStackTask {
 
    /**
@@ -12,19 +14,30 @@ public class MinStackTask {
     */
    static class MinStack {
 
+      private Stack<int[]> stack;
+
       public MinStack() {
+         stack = new Stack<>();
       }
 
       public void push(int val) {
+         if (stack.isEmpty()) {
+            stack.push(new int[]{val, val});
+         } else {
+            stack.push(new int[]{val, Math.min(stack.peek()[1], val)});
+         }
       }
 
       public void pop() {
+         stack.pop();
       }
 
-//      public int top() {
-//      }
-//
-//      public int getMin() {
-//      }
+      public int top() {
+         return stack.peek()[0];
+      }
+
+      public int getMin() {
+         return stack.peek()[1];
+      }
    }
 }
