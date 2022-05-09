a264bf0b6fd4db5c0f54f49b0bac7addb38ef6e9
ganzhab <bodaganj@gmail.com>
2022-05-09 11:44:13 +0200

    LRU cache is implemented with help of LHM

--- src/main/java/com/coding/tasks/leetcode/second_round/top_interview_questions/hard/design/LRUCacheTask.java
+++ src/main/java/com/coding/tasks/leetcode/second_round/top_interview_questions/hard/design/LRUCacheTask.java
@@ -1,6 +1,23 @@
 package com.coding.tasks.leetcode.second_round.top_interview_questions.hard.design;
 
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
 public class LRUCacheTask {
+
+   public static void main(String[] args) {
+      LRUCache o = new LRUCache(2);
+      o.put(1, 1);
+      o.put(2, 2);
+      System.out.println(o.get(1));
+      o.put(3, 3);
+      System.out.println(o.get(2));
+      o.put(4, 4);
+      System.out.println(o.get(1));
+      System.out.println(o.get(3));
+      System.out.println(o.get(4));
+   }
 
    /**
     * Your LRUCache object will be instantiated and called as such:
@@ -8,15 +25,28 @@ public class LRUCacheTask {
     * int param_1 = obj.get(key);
     * obj.put(key,value);
     */
-   static class LRUCache {
+   static class LRUCache extends LinkedHashMap<Integer, Integer> {
+
+      private int capacity;
 
       public LRUCache(int capacity) {
+         super(capacity, 0.75f, true);
+         this.capacity = capacity;
+         LinkedList<Integer> ll = new LinkedList<>();
+         ll.add(1, 3);
       }
 
-//      public int get(int key) {
-//      }
+      public int get(int key) {
+         return super.getOrDefault(key, -1);
+      }
 
       public void put(int key, int value) {
+         super.put(key, value);
+      }
+
+      @Override
+      public boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+         return super.size() > this.capacity;
       }
    }
 }
