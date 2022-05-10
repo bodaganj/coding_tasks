f8f0236a2f04311b999f9a23ab6533773af02536
ganzhab <bodaganj@gmail.com>
2022-05-10 16:37:16 +0200

    try to implement ser/deser BT with recursion

--- src/main/java/com/coding/tasks/leetcode/second_round/google/design/SerializeAndDeserializeBinaryTree.java
+++ src/main/java/com/coding/tasks/leetcode/second_round/google/design/SerializeAndDeserializeBinaryTree.java
@@ -13,15 +13,14 @@ public class SerializeAndDeserializeBinaryTree {
       n3.left = n4;
       n3.right = n5;
 
-//      TreeNode n1 = new TreeNode(1);
-
       pr(n1);
       System.out.println("============");
       Codec ser = new Codec();
       Codec deser = new Codec();
-//      String serialize = ser.serialize(n1);
-//      TreeNode ans = deser.deserialize(serialize);
-//      pr(ans);
+      String serialize = ser.serialize(n1);
+      System.out.println(serialize);
+      TreeNode ans = deser.deserialize(serialize);
+      pr(ans);
    }
 
    private static void pr(TreeNode root) {
@@ -36,16 +35,54 @@ public class SerializeAndDeserializeBinaryTree {
 
    static class Codec {
 
-//      public String serialize(TreeNode root) {
-//      }
-//
-//      public TreeNode deserialize(String data) {
-//      }
-//
-//      private TreeNode recDeser(LinkedList<String> list) {
-//      }
+      private int index = 0;
 
-      private void recSer(StringBuilder sb, TreeNode node) {
+      public String serialize(TreeNode root) {
+         StringBuilder sb = new StringBuilder();
+         recSer(root, sb);
+         return sb.substring(0, sb.length() - 1);
+      }
+
+      public TreeNode deserialize(String data) {
+         index = 0;
+         String[] split = data.split(",");
+         if (split.length < 2) {
+            return null;
+         } else {
+            TreeNode root = new TreeNode(Integer.parseInt(split[index]));
+            index++;
+            recDes(root, split);
+            return root;
+         }
+      }
+
+      private void recSer(TreeNode node, StringBuilder sb) {
+         if (node != null) {
+            sb.append(node.val).append(",");
+            recSer(node.left, sb);
+            recSer(node.right, sb);
+         } else {
+            sb.append("#,");
+         }
+      }
+
+      private void recDes(TreeNode node, String[] split) {
+         if (index < split.length) {
+            if (!split[index].equals("#")) {
+               TreeNode newNode = new TreeNode(Integer.parseInt(split[index++]));
+               node.left = newNode;
+               recDes(newNode, split);
+            } else {
+               index++;
+            }
+            if (!split[index].equals("#")) {
+               TreeNode newNode = new TreeNode(Integer.parseInt(split[index++]));
+               node.right = newNode;
+               recDes(newNode, split);
+            } else {
+               index++;
+            }
+         }
       }
    }
 
