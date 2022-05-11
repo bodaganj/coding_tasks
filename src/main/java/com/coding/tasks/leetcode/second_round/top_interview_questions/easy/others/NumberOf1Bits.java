a741d085e64a55ef5185205e4896075044b037b4
ganzhab <bodaganj@gmail.com>
2022-05-11 12:10:58 +0200

    NumberOf1Bits is implemented

--- src/main/java/com/coding/tasks/leetcode/second_round/top_interview_questions/easy/others/NumberOf1Bits.java
+++ src/main/java/com/coding/tasks/leetcode/second_round/top_interview_questions/easy/others/NumberOf1Bits.java
@@ -3,10 +3,22 @@ package com.coding.tasks.leetcode.second_round.top_interview_questions.easy.othe
 public class NumberOf1Bits {
 
    public static void main(String[] args) {
-//      System.out.println(hammingWeight(4));
-//      System.out.println(hammingWeight(7));
-//   }
-//
-//   private static int hammingWeight(int n) {
+      System.out.println(hammingWeight(4));
+      System.out.println(hammingWeight(7));
+      System.out.println(hammingWeight(-3));
+   }
+
+   private static int hammingWeight(int n) {
+      int counter = 0;
+      while (n != 0) {
+         int b = n ^ 1;
+
+         if (n > b) {
+            counter++;
+         }
+         b >>>= 1;
+         n = b;
+      }
+      return counter;
    }
 }
