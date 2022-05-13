978e08418c42cf91d93aba30a88869d5345c4078
ganzhab <bodaganj@gmail.com>
2022-05-13 17:13:51 +0200

    other tasks are finished

--- src/main/java/com/coding/tasks/leetcode/second_round/amazon/others/PartitionLabels.java
+++ src/main/java/com/coding/tasks/leetcode/second_round/amazon/others/PartitionLabels.java
@@ -1,12 +1,60 @@
 package com.coding.tasks.leetcode.second_round.amazon.others;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 public class PartitionLabels {
 
    public static void main(String[] args) {
-//      System.out.println(partitionLabels("ababcbacadefegdehijhklij"));
-//      System.out.println(partitionLabels("eccbbbbdec"));
-//   }
-//
-//   private static List<Integer> partitionLabels(String s) {
+      System.out.println(partitionLabels("ababcbacadefegdehijhklij"));
+      System.out.println(partitionLabels("eccbbbbdec"));
+   }
+
+   private static List<Integer> partitionLabels(String s) {
+      Map<Character, Integer> charsCount = new HashMap<>();
+      for (char aChar : s.toCharArray()) {
+         charsCount.put(aChar, charsCount.getOrDefault(aChar, 0) + 1);
+      }
+
+      List<Integer> ans = new ArrayList<>();
+      int i = 0;
+      int sum = 0;
+      Set<Character> visited = new HashSet<>();
+      while (i < s.length()) {
+         char currentChar = s.charAt(i);
+         visited.add(currentChar);
+
+         int newCounter = charsCount.get(currentChar) - 1;
+         if (newCounter == 0) {
+            charsCount.remove(currentChar);
+
+            if (checkIfAllVisitedCountZero(visited, charsCount)) {
+               int lengthOfPartition = i + 1 - sum;
+               ans.add(lengthOfPartition);
+               sum += lengthOfPartition;
+
+               visited = new HashSet<>();
+            }
+         } else {
+            charsCount.put(currentChar, newCounter);
+         }
+
+         i++;
+      }
+
+      return ans;
+   }
+
+   private static boolean checkIfAllVisitedCountZero(Set<Character> visited, Map<Character, Integer> charsCount) {
+      for (Character character : visited) {
+         if (charsCount.containsKey(character)) {
+            return false;
+         }
+      }
+      return true;
    }
 }
--- src/main/java/com/coding/tasks/leetcode/second_round/amazon/others/PrisonCellsAfterNDays.java
+++ src/main/java/com/coding/tasks/leetcode/second_round/amazon/others/PrisonCellsAfterNDays.java
@@ -1,11 +1,36 @@
 package com.coding.tasks.leetcode.second_round.amazon.others;
 
+import java.util.Arrays;
+
 public class PrisonCellsAfterNDays {
 
    public static void main(String[] args) {
-//      System.out.println(Arrays.toString(prisonAfterNDays(new int[]{0, 1, 0, 1, 1, 0, 0, 1}, 7)));
-//   }
-//
-//   private static int[] prisonAfterNDays(int[] cells, int n) {
+      System.out.println(Arrays.toString(prisonAfterNDays(new int[]{0, 1, 0, 1, 1, 0, 0, 1}, 40)));
+      prisonAfterNDays(new int[]{0, 1, 0, 1, 1, 0, 0, 1}, 40);
+   }
+
+   private static int[] prisonAfterNDays(int[] cells, int n) {
+      while (n > 14) {
+         n -= 14;
+      }
+
+      int[] dp = new int[cells.length];
+      for (int i = 0; i < n; i++) {
+         for (int j = 0; j < cells.length; j++) {
+            if (j == 0 || j == cells.length - 1) {
+               dp[j] = 0;
+            } else {
+               if (cells[j - 1] == cells[j + 1]) {
+                  dp[j] = 1;
+               } else {
+                  dp[j] = 0;
+               }
+            }
+         }
+
+         System.arraycopy(dp, 0, cells, 0, dp.length);
+         System.out.println(Arrays.toString(cells));
+      }
+      return cells;
    }
 }
