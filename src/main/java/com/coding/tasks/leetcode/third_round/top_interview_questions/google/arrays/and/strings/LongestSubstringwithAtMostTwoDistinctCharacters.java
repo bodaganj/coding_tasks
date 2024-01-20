5cd208ed6bf9ea21bfc5fe98cd1cf8e0d320609e
ganzhab <bodaganj@gmail.com>
2024-01-20 15:28:26 +0100

    implement several string/array google tasks

---
+++ src/main/java/com/coding/tasks/leetcode/third_round/top_interview_questions/google/arrays/and/strings/LongestSubstringwithAtMostTwoDistinctCharacters.java
@@ -0,0 +1,41 @@
+package com.coding.tasks.leetcode.third_round.top_interview_questions.google.arrays.and.strings;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LongestSubstringwithAtMostTwoDistinctCharacters {
+
+   public static void main(String[] args) {
+      System.out.println(lengthOfLongestSubstringTwoDistinct("eceba"));
+      System.out.println(lengthOfLongestSubstringTwoDistinct("ccaabbb"));
+   }
+
+   public static int lengthOfLongestSubstringTwoDistinct(String s) {
+      int longest = 0;
+
+      int right = 0;
+      int left = 0;
+      Map<Character, Integer> map = new HashMap<>();
+      while (right < s.length()) {
+         char currChar = s.charAt(right);
+         if (map.isEmpty() || map.containsKey(currChar) || map.keySet().size() < 2) {
+            map.put(currChar, map.getOrDefault(currChar, 0) + 1);
+            longest = Math.max(longest, right - left + 1);
+         } else {
+            map.put(currChar, 1);
+            while (map.size() == 3) {
+               char charAtLeft = s.charAt(left);
+               if (map.get(charAtLeft) == 1) {
+                  map.remove(charAtLeft);
+               } else {
+                  map.put(charAtLeft, map.get(charAtLeft) - 1);
+               }
+               left++;
+            }
+         }
+         right++;
+      }
+
+      return longest;
+   }
+}
---
+++ src/main/java/com/coding/tasks/leetcode/third_round/top_interview_questions/google/arrays/and/strings/MultiplyStrings.java
@@ -0,0 +1,58 @@
+package com.coding.tasks.leetcode.third_round.top_interview_questions.google.arrays.and.strings;
+
+import java.util.Objects;
+
+public class MultiplyStrings {
+
+   public static void main(String[] args) {
+      System.out.println(multiply("123", "456"));
+      System.out.println(multiply("9", "9"));
+      System.out.println(multiply("123456789", "987654321"));
+      System.out.println(multiply("498828660196", "840477629533"));
+      System.out.println(multiply("9133", "0"));
+   }
+
+   // length num1 - M
+   // length num2 - N
+   public static String multiply(String num1, String num2) {
+      if (Objects.equals(num1, "0") || Objects.equals(num2, "0")) {
+         return "0";
+      }
+      String product = "0";
+      int counter = 0;
+      for (int i = num1.length() - 1; i >= 0; i--) {
+         String curr = mult(num1.charAt(i) + "", num2) + "0".repeat(counter);
+         product = sum(product, curr);
+         counter++;
+      }
+
+      return product;
+   }
+
+   private static String mult(String small, String big) { // O(N)
+      int carry = 0;
+      StringBuilder sb = new StringBuilder();
+      for (int i = big.length() - 1; i >= 0; i--) {
+         int curr = Integer.parseInt(small) * Integer.parseInt(big.charAt(i) + "") + carry;
+         sb.append(curr % 10);
+         carry = curr / 10;
+      }
+      return carry != 0 ? "" + carry + sb.reverse() : sb.reverse().toString();
+   }
+
+   private static String sum(String first, String second) {
+      int carry = 0;
+      StringBuilder sb = new StringBuilder();
+      int length = Math.max(first.length(), second.length());
+      int i = 0;
+      while (i < length) {
+         int currFirst = i < first.length() ? Integer.parseInt(first.charAt(first.length() - 1 - i) + "") : 0;
+         int currSecond = i < second.length() ? Integer.parseInt(second.charAt(second.length() - 1 - i) + "") : 0;
+         int curr = currFirst + currSecond + carry;
+         sb.append(curr % 10);
+         carry = curr / 10;
+         i++;
+      }
+      return carry != 0 ? "" + carry + sb.reverse() : sb.reverse().toString();
+   }
+}
