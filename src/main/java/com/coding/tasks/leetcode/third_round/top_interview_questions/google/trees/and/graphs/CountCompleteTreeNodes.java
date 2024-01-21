6a1e7891980201fc5ca21994c73ed46bd57fdf22
ganzhab <bodaganj@gmail.com>
2024-01-21 22:14:18 +0100

    improve LongestIncreasingPathInMatrix

---
+++ src/main/java/com/coding/tasks/leetcode/third_round/top_interview_questions/google/trees/and/graphs/CountCompleteTreeNodes.java
@@ -0,0 +1,34 @@
+package com.coding.tasks.leetcode.third_round.top_interview_questions.google.trees.and.graphs;
+
+public class CountCompleteTreeNodes {
+
+   public static void main(String[] args) {
+
+   }
+
+//   public static int countNodes(TreeNode root) {
+//      // check left height
+//      // check right heigth
+//      // if equal -> return 2^k - 1
+//   }
+
+   public class TreeNode {
+
+      int val;
+      TreeNode left;
+      TreeNode right;
+
+      TreeNode() {
+      }
+
+      TreeNode(int val) {
+         this.val = val;
+      }
+
+      TreeNode(int val, TreeNode left, TreeNode right) {
+         this.val = val;
+         this.left = left;
+         this.right = right;
+      }
+   }
+}
--- src/main/java/com/coding/tasks/leetcode/third_round/top_interview_questions/hard/trees/and/graphs/LongestIncreasingPathInMatrix.java
+++ src/main/java/com/coding/tasks/leetcode/third_round/top_interview_questions/hard/trees/and/graphs/LongestIncreasingPathInMatrix.java
@@ -1,8 +1,5 @@
 package com.coding.tasks.leetcode.third_round.top_interview_questions.hard.trees.and.graphs;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class LongestIncreasingPathInMatrix {
 
    private static final int[][] dir = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
@@ -22,34 +19,27 @@ public class LongestIncreasingPathInMatrix {
       int[][] memo = new int[matrix.length][matrix[0].length];
       for (int i = 0; i < matrix.length; i++) {
          for (int j = 0; j < matrix[0].length; j++) {
-            Set<Pair> set = new HashSet<>();
-            set.add(new Pair(i, j));
-            dfs(i, j, set, memo);
+            dfs(i, j, memo);
          }
       }
       return max;
    }
 
-   private static int dfs(int i, int j, Set<Pair> seen, int[][] memo) {
+   private static int dfs(int i, int j, int[][] memo) {
       if (memo[i][j] > 0) {
          return memo[i][j];
       }
       int currMax = 1;
       for (int[] ints : dir) {
-         seen.add(new Pair(i, j));
          int x = i + ints[0];
          int y = j + ints[1];
-         if (x >= 0 && x < board.length && y >= 0 && y < board[0].length && board[x][y] > board[i][j] && !seen.contains(new Pair(x, y))) {
-            int dfs = 1 + dfs(x, y, new HashSet<>(seen), memo);
+         if (x >= 0 && x < board.length && y >= 0 && y < board[0].length && board[x][y] > board[i][j]) {
+            int dfs = 1 + dfs(x, y, memo);
             currMax = Math.max(currMax, dfs);
          }
       }
       max = Math.max(max, currMax);
       memo[i][j] = currMax;
       return memo[i][j];
-   }
-
-   public record Pair(int x, int y) {
-
    }
 }
