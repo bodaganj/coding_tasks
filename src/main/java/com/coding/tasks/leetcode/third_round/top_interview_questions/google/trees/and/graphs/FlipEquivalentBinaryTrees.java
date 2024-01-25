dad5cbb3bd75ee32343c7231f5ae8f727234b8ad
ganzhab <bodaganj@gmail.com>
2024-01-25 22:45:55 +0100

    finish all graph/tree google tasks

---
+++ src/main/java/com/coding/tasks/leetcode/third_round/top_interview_questions/google/trees/and/graphs/FlipEquivalentBinaryTrees.java
@@ -0,0 +1,40 @@
+package com.coding.tasks.leetcode.third_round.top_interview_questions.google.trees.and.graphs;
+
+public class FlipEquivalentBinaryTrees {
+
+   public static boolean flipEquiv(TreeNode root1, TreeNode root2) {
+      if (root1 == root2) {
+         return true;
+      }
+      if (root1 == null || root2 == null) {
+         return false;
+      }
+
+      if (root1.val != root2.val) {
+         return false;
+      }
+
+      return (flipEquiv(root1.left, root2.left) && flipEquiv(root1.right, root2.right)) ||
+         (flipEquiv(root1.right, root2.left) && flipEquiv(root1.left, root2.right));
+   }
+
+   public class TreeNode {
+
+      int val;
+      TreeNode left;
+      TreeNode right;
+
+      TreeNode() {
+      }
+
+      TreeNode(int val) {
+         this.val = val;
+      }
+
+      TreeNode(int val, TreeNode left, TreeNode right) {
+         this.val = val;
+         this.left = left;
+         this.right = right;
+      }
+   }
+}
