44dbbb55f032903aca4ff791f0a523aa03e8d9e4
bogdan <bogdan.ganzha@mend.io>
2024-01-28 22:50:53 +0100

    improvement 3.0

--- src/main/java/com/coding/tasks/leetcode/third_round/google/dynamic/programming/SplitArrayLargestSum.java
+++ src/main/java/com/coding/tasks/leetcode/third_round/google/dynamic/programming/SplitArrayLargestSum.java
@@ -1,5 +1,7 @@
 package com.coding.tasks.leetcode.third_round.google.dynamic.programming;
 
+import com.coding.tasks.cracking.the.code.recursion_and_dp.MaximumHeightByStackingCuboids;
+
 public class SplitArrayLargestSum {
 
   private static int max = Integer.MAX_VALUE;
@@ -10,13 +12,27 @@ public class SplitArrayLargestSum {
     System.out.println(splitArray(new int[]{1, 2, 3, 4, 5}, 2));
   }
 
-  private static void split(int currentIndex, int currentMax, int[] sum, int leftArrays) {
+  public static int splitArray(int[] nums, int k) {
+    max = Integer.MAX_VALUE;
+    int maxNumber = 0;
+    for (int num : nums) {
+      maxNumber = Math.max(maxNumber, num);
+    }
+
+    length = nums.length;
+    int[] sum = getSum(nums);
+    split(0, 0, sum, k, maxNumber);
+    return max;
+  }
+
+
+  private static void split(int currentIndex, int currentMax, int[] sum, int leftArrays, int maxNumber) {
     if (leftArrays == 1) {
       if (sum[length] - sum[currentIndex] > max) {
         return;
       } else {
         currentMax = Math.max(currentMax, sum[length] - sum[currentIndex]);
-        split(length, currentMax, sum, 0);
+        split(length, currentMax, sum, 0, maxNumber);
       }
     }
     if (leftArrays == 0 && currentIndex != length) {
@@ -28,23 +44,20 @@ public class SplitArrayLargestSum {
     if (leftArrays == 0) {
       max = currentMax;
     } else {
-      for (int i = currentIndex; i < length - (leftArrays - 1); i++) {
+      int i = currentIndex;
+      while (i < length - (leftArrays - 1)) {
         if (sum[i + 1] - sum[currentIndex] > max) {
           break;
         } else {
+          while (i + 2 <= length && sum[i + 2] - sum[currentIndex] < maxNumber) {
+            i++;
+          }
           currentMax = Math.max(currentMax, sum[i + 1] - sum[currentIndex]);
-          split(i + 1, currentMax, sum, leftArrays - 1);
+          split(i + 1, currentMax, sum, leftArrays - 1, maxNumber);
         }
+        i++;
       }
     }
-  }
-
-  public static int splitArray(int[] nums, int k) {
-    max = Integer.MAX_VALUE;
-    length = nums.length;
-    int[] sum = getSum(nums);
-    split(0, 0, sum, k);
-    return max;
   }
 
   private static int[] getSum(int[] nums) {
