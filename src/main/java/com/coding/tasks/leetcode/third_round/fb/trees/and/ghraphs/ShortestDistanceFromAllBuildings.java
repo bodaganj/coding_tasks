ffdaa7b6b28c2777e3d64657119ae5e0825abeda
bogdan <bogdan.ganzha@mend.io>
2024-02-10 22:21:23 +0100

    TLE with dfs achieved

--- src/main/java/com/coding/tasks/leetcode/third_round/fb/trees/and/ghraphs/ShortestDistanceFromAllBuildings.java
+++ src/main/java/com/coding/tasks/leetcode/third_round/fb/trees/and/ghraphs/ShortestDistanceFromAllBuildings.java
@@ -5,8 +5,8 @@ import java.util.*;
 public class ShortestDistanceFromAllBuildings {
 
    private static int[][] dirs = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-   private static Map<List<Pair>, Integer> memo = new HashMap<>();
    private static int shortest = Integer.MAX_VALUE;
+   private static int[][] matrix;
 
    public static void main(String[] args) {
       int[][] grid1 = new int[][]{
@@ -23,15 +23,15 @@ public class ShortestDistanceFromAllBuildings {
    }
 
    public static int shortestDistance(int[][] grid) {
-      memo = new HashMap<>();
+      matrix = grid;
       shortest = Integer.MAX_VALUE;
       List<Pair> emptySlots = new ArrayList<>();
       List<Pair> buildings = new ArrayList<>();
-      for (int i = 0; i < grid.length; i++) {
+      for (int i = 0; i < matrix.length; i++) {
          for (int j = 0; j < grid[0].length; j++) {
-            if (grid[i][j] == 0) {
+            if (matrix[i][j] == 0) {
                emptySlots.add(new Pair(i, j));
-            } else if (grid[i][j] == 1) {
+            } else if (matrix[i][j] == 1) {
                buildings.add(new Pair(i, j));
             }
          }
@@ -41,7 +41,7 @@ public class ShortestDistanceFromAllBuildings {
       }
 
       for (Pair emptySlot : emptySlots) {
-         int currentShortest = getShortest(emptySlot, buildings, grid);
+         int currentShortest = getShortest(emptySlot, buildings);
          if (currentShortest > 0) {
             shortest = Math.min(shortest, currentShortest);
          }
@@ -49,30 +49,26 @@ public class ShortestDistanceFromAllBuildings {
       return shortest == Integer.MAX_VALUE ? -1 : shortest;
    }
 
-   private static int getShortest(Pair emptySlot, List<Pair> allBuildings, int[][] grid) {
+   private static int getShortest(Pair emptySlot, List<Pair> allBuildings) {
       int sum = 0;
       for (Pair building : allBuildings) {
          Set<Pair> set = new HashSet<>();
          set.add(emptySlot);
-         int distance = getDistanceToBuilding(emptySlot, building, grid, 1, memo, set);
-         if (distance == 0) {
-            return 0;
-         } else {
+         int distance = getDistanceToBuilding(emptySlot, building, 0, set);
+         if (distance > 0) {
             sum += distance;
+         } else {
+            return -1;
          }
       }
       return sum;
    }
 
-   private static int getDistanceToBuilding(Pair nextSlot, Pair building, int[][] grid, int count, Map<List<Pair>, Integer> memo, Set<Pair> seen) {
-      if (memo.containsKey(List.of(nextSlot, building))) {
-         return memo.get(List.of(nextSlot, building));
-      }
-
+   private static int getDistanceToBuilding(Pair nextSlot, Pair building, int count, Set<Pair> seen) {
       if (nextSlot.equals(building)) {
          return count;
-      } else if (grid[nextSlot.x][nextSlot.y] == 1){
-         return 0;
+      } else if (matrix[nextSlot.x][nextSlot.y] == 1){
+         return -1;
       } else {
          int currentMin = Integer.MAX_VALUE;
 
@@ -80,18 +76,16 @@ public class ShortestDistanceFromAllBuildings {
             int x = nextSlot.x + dir[0];
             int y = nextSlot.y + dir[1];
             Pair pair = new Pair(x, y);
-            if (x >= 0 && x < grid.length && y >= 0 && y < grid[0].length && grid[x][y] != 2 && !seen.contains(pair)) {
+            if (x >= 0 && x < matrix.length && y >= 0 && y < matrix[0].length && matrix[x][y] != 2 && !seen.contains(pair)) {
                seen.add(pair);
-               int distanceToBuilding = getDistanceToBuilding(pair, building, grid, count + 1, memo, seen);
+               int distanceToBuilding = getDistanceToBuilding(pair, building, count + 1, seen);
                if (distanceToBuilding > 0) {
-                  currentMin = Math.min(currentMin, count + distanceToBuilding);
+                  currentMin = Math.min(currentMin, distanceToBuilding);
                }
                seen.remove(pair);
             }
          }
-         memo.put(List.of(nextSlot, building), currentMin);
-         System.out.println("Path from " + nextSlot.x + ":" + nextSlot.y + " to building " + building.x + ":" + building.y + " is - " + currentMin);
-         return currentMin;
+         return currentMin == Integer.MAX_VALUE ? -1 : currentMin;
       }
    }
 
