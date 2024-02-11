c0ab5b5bb0ec10816d8f0685f37695432038c4eb
ganzhab <bodaganj@gmail.com>
2024-02-11 15:32:42 +0100

    finish FB graphs and trees tasks

--- src/main/java/com/coding/tasks/leetcode/third_round/fb/trees/and/ghraphs/BinaryTreeVerticalOrderTraversal.java
+++ src/main/java/com/coding/tasks/leetcode/third_round/fb/trees/and/ghraphs/BinaryTreeVerticalOrderTraversal.java
@@ -27,9 +27,8 @@ public class BinaryTreeVerticalOrderTraversal {
       if (root == null) {
          return Collections.emptyList();
       }
-
       Map<Integer, List<Pair>> map = new TreeMap<>();
-      rec(root, 0, 0, map);
+      dfs(root, 0, 0, map);
 
       return map.values()
                 .stream()
@@ -40,14 +39,14 @@ public class BinaryTreeVerticalOrderTraversal {
                 .collect(Collectors.toList());
    }
 
-   private static void rec(TreeNode node, int i, int level, Map<Integer, List<Pair>> map) {
+   private static void dfs(TreeNode node, int i, int level, Map<Integer, List<Pair>> map) {
       if (node != null) {
          if (!map.containsKey(i)) {
             map.put(i, new ArrayList<>());
          }
          map.get(i).add(new Pair(node.val, level));
-         rec(node.left, i - 1, level + 1, map);
-         rec(node.right, i + 1, level + 1, map);
+         dfs(node.left, i - 1, level + 1, map);
+         dfs(node.right, i + 1, level + 1, map);
       }
    }
 
